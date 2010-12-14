81dee77113cddbe531545da2f0a1532ff7c70980
Darlene E McCullough <darlene.mccullough@mail.cuny.edu>
2010-12-14 15:03:04 -0500

    updating text on home and about indexSuccess pages

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -1,8 +1,7 @@
-        <p>Welcome to the Office of Emergency Management's NYC Coastal Storm Plan's web application. This Agasti installation will allow you to manage staff, resources, clients and facilities through an easy to use web interface.</p>
-        <?php
-          if (!$sf_user->isAuthenticated())
-          {
-            echo "<p>Please login on the top right.</p>";
-          }
-        ?>
-        <p>Built with PHP, MySQL, Doctrine ORM and Symfony.</p>
+<p>Welcome to the Office of Emergency Management's NYC Coastal Storm Plan's web application, Sahana Agasti. Sahana Agasti is an emergency management application that helps to manage staff, resources, clients and facilities through an easy to use web interface.  </p>
+<?php
+if (!$sf_user->isAuthenticated()) {
+  echo "<p>Please login on the top right.</p>";
+}
+?>
+<p>Built with PHP, MySQL, Doctrine ORM and Symfony.</p>
