cd7d598e10429cbd6fb7e88c0d3e17cc1f389887
Usman Akeju <usman.akeju@mail.cuny.edu>
2011-01-10 14:02:51 -0500

    - adds Foo action and foo menu items

---
+++ apps/frontend/templates/listFooSuccess.php
@@ -0,0 +1,11 @@
+<?php
+$columns = array(
+    'id' => array('title' => 'Id', 'sortable' => false),
+    'foo' => array('title' => 'Name', 'sortable' => true),
+    'bar' => array('title' => 'Drink', 'sortable' => true),
+  );
+
+//pager comes in from the action
+
+echo agListForm::facilitylist($sf_request,'Foo Listing', $columns, $pager);
+?>
