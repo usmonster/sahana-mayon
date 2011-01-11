187c1131b29e73f992d8ba1dcb9fe5865b9f2ab3
charles wisniewski <charles.wisniewski@mail.cuny.edu>
2011-01-11 12:43:12 -0500

    gis settings are now pulled from the global parameter table

--- plugins/agGisPlugin/modules/agGis/templates/configSuccess.php
+++ plugins/agGisPlugin/modules/agGis/templates/configSuccess.php
@@ -7,4 +7,11 @@
 ?>
 <h3>Configure GIS Settings</h3>
 
-<h4>enter API Url (e.g)</h4>
+<h4>Map URL</h4>
+<?php echo $map_url->getValue();?>
+<br>
+
+<a href="<?php echo url_for('admin/config?param='.$map_url->getId()) ?>">edit</a>
+
+
+
