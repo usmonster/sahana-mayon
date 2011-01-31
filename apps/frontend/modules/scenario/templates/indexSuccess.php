4f85b9fc76a1e7a523c7fc2f54a104552440eb93
Darlene E McCullough <darlene.mccullough@mail.cuny.edu>
2011-01-31 14:27:29 -0500

    updated scenario index links

--- apps/frontend/modules/scenario/templates/indexSuccess.php
+++ apps/frontend/modules/scenario/templates/indexSuccess.php
@@ -10,8 +10,5 @@
 <?php
 echo '<a href="' . url_for('scenario/pre') . '" class="buttonText" title="Create New Scenario">Create Scenario<a/><br/>';
 echo '<a href="' . url_for('scenario/list') . '" class="buttonText" title="List Existing Scenarios">List Scenarios</a><br/>';
-echo '<a href="' . url_for('scenario/listshifttemplate') . '" class="buttonText" title="List Shift Template">List Shift Template</a><br/>';
-echo '<a href="' . url_for('scenario/scenarioshiftlist') . '" class="buttonText" title="List Scenario Shift">List Scenario Shift</a><br/>';
-echo '<a href="' . url_for('scenario/scenarioshiftgroup') . '" class="buttonText" title="List Scenario Shift">Delete Scenario Shifts By Scenario</a><br/>';
 echo '<a href="' . public_path('wiki/doku.php?id=manual:user:scenario') . '" target="new" class="buttonText" title="Help">Help</a><br/>';
 ?><br>
