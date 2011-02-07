881b16c4d60f679536864a81e8eaa7f59bdb1363
Usman Akeju <usman.akeju@mail.cuny.edu>
2011-02-07 16:39:42 -0500

    - fixes IE9.js include

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -8,7 +8,7 @@
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?> 
     <!--[if lt IE 9]>
-    <script src="<?php echo url_for('js/IE9.js') ?>"></script>
+    <?php echo javascript_include_tag('IE9'); ?>
     <![endif]-->
   </head>
   <body>
