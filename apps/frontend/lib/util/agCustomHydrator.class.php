d9ede904b1540131506d654e3a1766609032e446
Chad Heuschober <chad.heuschober@mail.cuny.edu>
2011-02-10 14:25:52 -0500

    Added key_value_array hydrator.

--- apps/frontend/lib/util/agCustomHydrator.class.php
+++ apps/frontend/lib/util/agCustomHydrator.class.php
@@ -14,7 +14,7 @@
 class KeyValuePairHydrator extends Doctrine_Hydrator_Abstract
 {
   /**
-   * Defines the result set as an associate array.
+   * Defines the result set as an associative array.
    * 
    * @param <type> $stmt
    * @return array An associate array.
@@ -26,6 +26,38 @@ class KeyValuePairHydrator extends Doctrine_Hydrator_Abstract
     foreach ($results as $result)
     {
       $array[$result[0]] = $result[1];
+    }
+
+    return $array;
+  }
+}
+
+/**
+ * KeyValuePairHydrator this class extends the Doctrine_Hydrator_Abstract class.
+ *
+ * LICENSE: This source file is subject to LGPLv3.0 license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/copyleft/lesser.html
+ *
+ * @author     Shirley Chan, CUNY SPS
+ *
+ * Copyright of the Sahana Software Foundation, sahanafoundation.org
+ */
+class KeyValueArrayHydrator extends Doctrine_Hydrator_Abstract
+{
+  /**
+   * Defines the result set as an associative array.
+   *
+   * @param <type> $stmt
+   * @return array An associative array.
+   */
+  public function hydrateResultSet($stmt)
+  {
+    $results = $stmt->fetchAll(Doctrine_Core::FETCH_NUM);
+    $array = array();
+    foreach ($results as $result)
+    {
+      $array[$result[0]] = array_slice($result, -1, -1);
     }
 
     return $array;
@@ -48,6 +80,27 @@ class GisPointCoordinateHydrator extends Doctrine_Hydrator_Abstract
     {
       $array[] = array($result[0], $result[3], $result[4]);
     } 
+
+    return $array;
+  }
+}
+
+class StatusHydrator extends Doctrine_Hydrator_Abstract
+{
+  /**
+   * Defines the result set as a nested associative array.
+   *
+   * @param <type> $stmt
+   * @return array An associative array.
+   */
+  public function hydrateResultSet($stmt)
+  {
+    $results = $stmt->fetchAll(Doctrine_Core::FETCH_NUM);
+    $array = array();
+    foreach ($results as $result)
+    {
+      $array[$result[0]] = array($result[1], $result[2]);
+    }
 
     return $array;
   }
