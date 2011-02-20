a08ed34bb2825cdaf1bc6837cf34fc1c183c2e89
Chad Heuschober <chad.heuschober@mail.cuny.edu>
2011-02-20 18:56:54 -0500

    Updated the schema for facility resource allocation status. NOTE: this likely breaks some fixtures.

---
+++ lib/filter/doctrine/temp_import_migrationFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * temp_import_migration filter form.
+ *
+ * @package    AGASTI_CORE
+ * @subpackage filter
+ * @author     CUNY SPS
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class temp_import_migrationFormFilter extends Basetemp_import_migrationFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/temp_import_migrationForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * temp_import_migration form.
+ *
+ * @package    AGASTI_CORE
+ * @subpackage form
+ * @author     CUNY SPS
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class temp_import_migrationForm extends Basetemp_import_migrationForm
+{
+  public function configure()
+  {
+  }
+}
