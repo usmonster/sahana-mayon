732533b29af6c15b1db22b32f9b6d2ec9f0a5d36
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-02-28 15:23:59 -0500

    Updated menu with rounded corners by adding jquery

---
+++ web/js/Menu_Shape.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function() {
+
+   $("ul.menu").css("-moz-border-radius" , "0px 0px 15px 15px");
+   $("ul.menu li.menu1 a:first").css("-moz-border-radius" , "0px 0px 0px 15px");
+   $("ul.menu li.menu1 a:last").css("-moz-border-radius" , "0px 0px 15px 0px");
+
+})
