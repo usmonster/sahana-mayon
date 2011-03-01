1d5d00df0cb3689fca76c27b7fc7a4477d89bc88
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-03-01 13:26:51 -0500

    included fancy Search Box

--- apps/frontend/modules/search/templates/_searchForm.php
+++ apps/frontend/modules/search/templates/_searchForm.php
@@ -1,6 +1,8 @@
 <?php use_helper('I18N') ?>
 <form action="<?php echo url_for($sf_request->getParameter('module') . '/search') ?>" method="get" class="displayInline">
-  <input type="text" name="query" value="<?php echo $sf_request->getParameter('query'); ?>"
+  <!--<input type="text" name="query" value="<?php echo $sf_request->getParameter('query'); ?>"
          id="search_keywords" class="searchForm"/>
-  <input type="submit" value="Search" class="buttonWhite" />
+  <input type="submit" value="Search" class="buttonWhite" />-->
+  <input type="text" name="query" value="<?php echo $sf_request->getParameter('query'); ?>"
+         id="search_keywords" style="border: 1px solid #ccc; display: inline; margin-top:3px;vertical-align: top; width:160px; height: 19px"/><input type="image" src="<?php echo url_for('images/Search_Button.png') ?>" value="Search" style="display:inline; margin-top:3px" />
 </form>
