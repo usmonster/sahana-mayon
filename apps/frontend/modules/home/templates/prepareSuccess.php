54402ddae870d37930ef32c6bc631c2d6348c98a
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-03-16 13:06:16 -0400

    Added Prepare page which includes short cut links

---
+++ apps/frontend/modules/home/templates/prepareSuccess.php
@@ -0,0 +1,19 @@
+<h2>Preparedness</h2>
+<p>...........Some content goes here.............</p>
+<b>Please select one of the following actions: </b>
+<table cellspacing="20">
+<tr>
+    <td><a href="<?php echo url_for('staff/index'); ?>" class="linkButton" style="width:140px;" title="Manage Staff">Manage<br>Staff</a></td>
+    <td><a href="<?php echo url_for('faculty/index'); ?>" class="linkButton"  style="width:140px" title="Manage Faculty">Manage<br>Faculty</a></td>
+    <td><a href="<?php echo url_for('organization/index'); ?>" class="linkButton"  style="width:140px" title="Manage Organization">Manage<br>Organization</a></td>
+    <td><a href="<?php echo url_for('scenario/index'); ?>" class="linkButton" style="width:140px"  title="Deploy a Scenario">Deploy a<br>Scenario</a></td>
+</tr>
+<tr>
+    <td><a href="<?php echo url_for('scenario/pre'); ?>" class="linkButton" style="width:140px"  title="Create New Scenario">Create<br>New Scenario</a></td>
+    <td><a href="<?php echo url_for('wiki/doku.php?id=manual:user:scenario:walkthrough'); ?>" class="linkButton"  style="width:140px" title="Scenario Creator Walkthrough">Scenario Creator<br>Walkthrough</a></td>
+    <td><a href="<?php echo url_for('scenario/list'); ?>" class="linkButton"  style="width:140px" title="Manage Existing Scenarios">Manage<br>Existing Scenarios</a></td>
+    <td></td>
+</tr>
+</table>
+
+<br><br><br><br>
