d1c887b9a5ca206bc6d7d88e8189c7d846ac03a5
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-03-16 16:11:25 -0400

    replaced url_for to link_to

--- apps/frontend/modules/home/templates/prepareSuccess.php
+++ apps/frontend/modules/home/templates/prepareSuccess.php
@@ -2,18 +2,19 @@
 <p>...........Some content goes here.............</p>
 <b>Please select one of the following actions: </b>
 <table cellspacing="20">
-<tr>
-    <td><a href="<?php echo url_for('staff/index'); ?>" class="linkButton" style="width:140px;" title="Manage Staff">Manage<br>Staff</a></td>
-    <td><a href="<?php echo url_for('facility/index'); ?>" class="linkButton"  style="width:140px" title="Manage Facility">Manage<br>Facility</a></td>
-    <td><a href="<?php echo url_for('organization/index'); ?>" class="linkButton"  style="width:140px" title="Manage Organization">Manage<br>Organization</a></td>
-    <td><a href="<?php echo url_for('scenario/index'); ?>" class="linkButton" style="width:140px"  title="Deploy a Scenario">Deploy a<br>Scenario</a></td>
-</tr>
-<tr>
-    <td><a href="<?php echo url_for('scenario/pre'); ?>" class="linkButton" style="width:140px"  title="Create New Scenario">Create<br>New Scenario</a></td>
-    <td><a href="<?php echo url_for('wiki/doku.php?id=manual:user:scenario:walkthrough'); ?>" class="linkButton"  style="width:140px" title="Scenario Creator Walkthrough">Scenario Creator<br>Walkthrough</a></td>
-    <td><a href="<?php echo url_for('scenario/list'); ?>" class="linkButton"  style="width:140px" title="Manage Existing Scenarios">Manage<br>Existing Scenarios</a></td>
-    <td></td>
-</tr>
+    <tr>
+      <td><?php echo link_to('Manage<br>Staff', 'staff/index', array('class' => 'linkButton', 'style' => 'width:140px')) ?></td>
+      <td><?php echo link_to('Manage<br>Facility', 'facility/index', array('class' => 'linkButton', 'style' => 'width:140px')) ?></td>
+      <td><?php echo link_to('Manage<br>Organization', 'organization/index', array('class' => 'linkButton', 'style' => 'width:140px')) ?></td>
+      <td><?php echo link_to('Deploy a<br>Scenario', 'scenario/index', array('class' => 'linkButton', 'style' => 'width:140px')) ?></td>
+    </tr>
+    <tr>
+      <td><?php echo link_to('Create<br>New Scenario', 'scenario/pre', array('class' => 'linkButton', 'style' => 'width:140px')) ?></td>
+      <td><?php echo link_to('Scenario Creator<br>Walkthrough', 'wiki/doku.php?id=manual:user:scenario:walkthrough', array('class' => 'linkButton', 'style' => 'width:140px')) ?></td>
+      <td><?php echo link_to('Manage<br>Existing Scenarios', 'scenario/list', array('class' => 'linkButton', 'style' => 'width:140px')) ?></td>
+      <td></td>
+    </tr>
 </table>
 
 <br><br><br><br>
+
