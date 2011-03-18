ea875caa4486a0ab2c522e36acfa585a0d3436e3
Shirley Chan <shirley.chan@mail.cuny.edu>
2011-03-18 00:47:21 -0400

    More helper classes for email and phone.

---
+++ apps/frontend/lib/util/agEntityPhoneHelper.class.php
@@ -0,0 +1,296 @@
+<?php
+/**
+ * Provides entity phone helper functions and inherits several methods and properties from the
+ * bulk record helper.
+ *
+ * PHP Version 5
+ *
+ * LICENSE: This source file is subject to LGPLv3.0 license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/copyleft/lesser.html
+ *
+ * @author Shirley Chan, CUNY SPS
+ *
+ * Copyright of the Sahana Software Foundation, sahanafoundation.org
+ */
+class agEntityPhoneHelper extends agBulkRecordHelper
+{
+  public    $agPhoneHelper,
+            $defaultIsPrimary = FALSE,
+            $defaultIsStrType = FALSE;
+
+  protected $_batchSizeModifier = 2;
+
+  /**
+   * Method to lazily load the $agPhoneHelper class property (an instance of agPhoneHelper)
+   * @return object The instantiated agPhoneHelper object
+   */
+  public function getAgPhoneHelper()
+  {
+    if (! isset($this->agPhoneHelper)) { $this->agPhoneHelper = agAddressHelper::init() ; }
+    return $this->agPhoneHelper ;
+  }
+
+  /**
+   * Method to return an agDocrineQuery object, preconfigured to collect entity phones.
+   *
+   * @param array $entityIds A single dimension array of entityId's that will be queried.
+   * @param boolean $strType Boolean that determines whether or not the phone contact type will
+   * be returned as an ID value or its string equivalent.
+   * @return Doctrine_Query An agDoctrineQuery object.
+   */
+  private function _getEntityPhoneQuery($entityIds = NULL, $strType = NULL)
+  {
+    // if no (null) ID's are passed, get the entityIds from the class property
+    $entityIds = $this->getRecordIds($entityIds);
+
+    // if strType is not passed, get the default
+    if (is_null($strType)) { $strType = $this->defaultIsStrType; }
+
+    // the most basic version of this query
+    $q = agDoctrineQuery::create()
+       ->select('epc.entity_id')
+         ->addSelect('epc.phone_contact_id')
+       ->from('agEntityPhoneContact epc')
+       ->whereIn('epc.entity_id', $entityIds)
+       ->orderBy('epc.priority');
+
+    // here we determine whether to return the phone_contact_type_id or its string value
+    if ($strType)
+    {
+      $q->addSelect('pct.phone_contact_type')
+        ->innerJoin('epc.agPhoneContactType pct');
+    } else {
+      $q->addSelect('epc.phone_contact_type_id');
+    }
+
+    return $q;
+  }
+
+  /**
+   * Method to return entity phones for a group of entity ids, sorted from highest priority to
+   * lowest priority, and grouped by the phone contact type.
+   *
+   * @param array $entityIds A single dimension array of entityId's that will be queried.
+   * @param boolean $strType Boolean that determines whether or not the phone contact type will
+   * be returned as an ID value or its string equivalent.
+   * @param boolean $primary Boolean that determines whether or not only the primary phone will
+   * be returned (for that type).
+   * @param boolean $formatPhone Boolean that determines whether or not to format the phone.
+   * @param string $phoneHelperMethod The phone helper method that will be called to format or
+   * process the phones. This should be an agPhoneHelper::PHN_GET_* constant. If left NULL,
+   * only phone ID's will be returned.
+   * @param array $phoneArgs An array of arguments to pass forward to the phone helper.
+   * @return array A two or three dimensional array (depending on the setting of the $primary
+   * parameter), by entityId, by phoneContactType.
+   */
+  public function getEntityPhoneByType ($entityIds = NULL,
+                                        $strType = NULL,
+                                        $primary = NULL,
+                                        $phoneHelperMethod = NULL,
+                                        $phoneArgs = array())
+  {
+    // initial results declarations
+    $entityPhones = array();
+
+    // if primary is not passed, get the default
+    if (is_null($primary)) { $primary = $this->defaultIsPrimary; }
+
+    // build our query object
+    $q = $this->_getEntityPhoneQuery($entityIds, $strType);
+
+    // if this is a primary query we add the restrictor
+    if ($primary)
+    {
+      $q->addWhere(' EXISTS (
+        SELECT s.id
+          FROM agEntityPhoneContact s
+          WHERE s.entity_id = epc.entity_id
+            AND s.phone_contact_type_id = epc.phone_contact_type_id
+          HAVING MIN(s.priority) = epc.priority )') ;
+    }
+
+    // build this as custom hydration to 'double tap' the data
+    $rows = $q->execute(array(), Doctrine_Core::HYDRATE_NONE);
+    $index = 0;
+    $priorEntityId = '';
+    $priorContactType = '';
+    foreach ($rows as $row)
+    {
+
+      // if we're only returning the primary, change the third dimension from an array to a value
+      // NOTE: because of the restricted query, we can trust there is only one component per type
+      // in our output and safely make this assumption
+      if ($primary)
+      {
+        $entityPhones[$row[0]][$row[2]][] = $row[1];
+      }
+      // if not primary, we have one more loop in our return for another array nesting
+      else {
+        if ($row[0] != $priorEntityId || $row[2] != $priorContactType) { $index = 0; }
+        $entityPhones[$row[0]][$row[2]][$index++] = $row[1];
+        $priorEntityId = $row[0];
+        $priorContactType = $row[2];
+      }
+    }
+
+    // if no phone helper method was passed, assume that all we need are the phone id's and
+    // stop right here!
+    if (is_null($phoneHelperMethod))
+    {
+      return $entityPhones ;
+    }
+
+    // otherwise... we keep going and lazily load our phone helper, 'cause we'll need her
+    $phoneHelper = $this->getAgPhoneHelper() ;
+
+    // finish appending the rest of our address helper args
+    foreach ($phoneArgs as $arg)
+    {
+      $phoneHelperArgs[] = $arg ;
+    }
+
+    // use the phone helper to format the phone results
+    $userFunc = array($phoneHelper,$phoneHelperMethod) ;
+    $formattedPhones = call_user_func_array($userFunc,$phoneHelperArgs) ;
+
+    // we can release the phone helper args, since we don't need them anymore
+    unset($phoneHelperArgs) ;
+
+    // now loop through our entities and replace phone value with formatted phone.
+    foreach ($entityPhones as $entityId => $phoneTypes)
+    {
+      foreach ($phoneTypes as $phoneType => $phones)
+      {
+        // if we're only returning the primary, change the third dimension from an array to a value
+        // NOTE: because of the restricted query, we can trust there is only one component per type
+        // in our output and safely make this assumption
+        if ($primary)
+        {
+          $entityPhones[$entityId][$phoneType] = $formattedPhone[$phones[0]];
+        }
+        // if not primary, we have one more loop in our return for another array nesting
+        else
+        {
+          foreach ($phones as $index => $phone)
+          {
+            $entityPhones[$entityId][$phoneType][$index] = $formattedAddresses[$phone] ;
+          }
+        }
+      }
+    }
+
+    return $entityPhones ;
+
+  }
+
+  /**
+   * Method to return entity phones for a group of entity ids, sorted from highest priority to
+   * lowest priority.
+   *
+   * @param array $entityIds A single dimension array of entityId's that will be queried.
+   * @param boolean $strType Boolean that determines whether or not the phone contact type will
+   * be returned as an ID value or its string equivalent.
+   * @param boolean $primary Boolean that determines whether or not only the primary phone will
+   * be returned (for that type).
+   * @param string $phoneHelperMethod The phone helper method that will be called to format or
+   * process the phones. This should be an agPhoneHelper::PHN_GET_* constant. If left NULL,
+   * only phone ID's will be returned.
+   * @param array $phoneArgs An array of arguments to pass forward to the phone helper.
+   * @return array A three dimensional array, by entityId, then indexed from highest priority
+   * phone to lowest, with a third dimension containing the email type as index[0], and the
+   * phone value as index[1].
+   */
+  public function getEntityPhone ($entityIds = NULL,
+                                  $strType = NULL,
+                                  $primary = NULL,
+                                  $phoneHelperMethod = NULL,
+                                  $phoneArgs = array())
+  {
+    // initial results declarations
+    $entityPhones = array();
+
+    // if primary is not passed, get the default
+    if (is_null($primary)) { $primary = $this->defaultIsPrimary; }
+
+    // build our query object
+    $q = $this->_getEntityPhoneQuery($entityIds, $strType);
+
+    // if this is a primary query we add the restrictor
+    if ($primary)
+    {
+      $q->addWhere(' EXISTS (
+        SELECT s.id
+          FROM agEntityPhoneContact s
+          WHERE s.entity_id = epc.entity_id
+          HAVING MIN(s.priority) = epc.priority )');
+    }
+    // build this as custom hydration to 'double tap' the data
+    $rows = $q->execute(array(), Doctrine_Core::HYDRATE_NONE);
+
+    $index = 0;
+    $priorEntityId = '';
+    $priorContactType = '';
+    foreach ($rows as $row)
+    {
+      // if we're only returning the primary, change the second dimension from an array to a value
+      // NOTE: because of the restricted query, we can trust there is only one component per type
+      // in our output and safely make this assumption
+      if ($primary) {
+        $entityPhones[$row[0]][]= array($row[2],$row[1]);
+      }
+      // if not primary, we have one more loop in our return for another array nesting
+      else {
+        if ($row[0] != $priorEntityId || $row[2] != $priorContactType) { $index = 0; }
+        $entityPhones[$row[0]][$index++] = array($row[2], $row[1]);
+      }
+
+    }
+
+    // if no phone helper method was passed, assume that all we need are the phone id's and
+    // stop right here!
+    if (is_null($phoneHelperMethod))
+    {
+      return $entityPhones ;
+    }
+
+    // otherwise... we keep going and lazily load our phone helper, 'cause we'll need her
+    $phoneHelper = $this->getAgPhoneHelper() ;
+
+    // finish appending the rest of our address helper args
+    foreach ($phoneArgs as $arg)
+    {
+      $phoneHelperArgs[] = $arg ;
+    }
+
+    // use the phone helper to format the phone results
+    $userFunc = array($phoneHelper,$phoneHelperMethod) ;
+    $formattedPhones = call_user_func_array($userFunc,$phoneHelperArgs) ;
+
+    // we can release the phone helper args, since we don't need them anymore
+    unset($phoneHelperArgs) ;
+
+    // now loop through our entities and replace phone value with formatted phone.
+    foreach ($entityPhones as $entityId => $phones)
+    {
+      // if we're only returning the primary, change the second dimension from an array to a value
+      // NOTE: because of the restricted query, we can trust there is only one component per type
+      // in our output and safely make this assumption
+      if ($primary)
+      {
+        $entityPhones[$entityId] = array($phones[0][0],$formattedPhones[$phones[0][1]]) ;
+      }
+      // if not primary, we have one more loop in our return for another array nesting
+      else
+      {
+        foreach ($phones as $index => $phone)
+        {
+          $entityPhones[$entityId][$index][1] = array($formattedPhones[$phone[1]]) ;
+        }
+      }
+    }
+
+    return $entityPhones;
+  }
+
+}
---
+++ apps/frontend/lib/util/agPhoneHelper.class.php
@@ -0,0 +1,212 @@
+<?php
+/**
+ *
+ * Provides bulk-phone manipulation methods
+ *
+ * PHP Version 5
+ *
+ * LICENSE: This source file is subject to LGPLv3.0 license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/copyleft/lesser.html
+ *
+ * @author Shirley Chan, CUNY SPS
+ *
+ * Copyright of the Sahana Software Foundation, sahanafoundation.org
+ */
+class agPhoneHelper extends agBulkRecordHelper
+{
+  // these constants map to the phone get types that are supported in other function calls
+  const     PHN_GET_FORMATTED = 'getFormattedPhone';
+
+  protected $_globalDefaultPhoneFormatType = 'default_phone_format_type',
+            $_phoneValidation = array(),
+            $_phoneDisplayFormat = array(),
+            $_returnFormatId;
+
+  /**
+   * This is the class's constructor whic pre-loads the formatting elements according to the default
+   * return format.
+   *
+   * @param array $phoneIds A single dimension array of phone id values.
+   */
+  public function __construct($phoneIds = NULL)
+  {
+    // if passed an array of phone id's, set them as a class property
+    parent::__construct($phoneIds);
+
+    // set our default phone standard and pick up the formatting components
+    $this->_setDefaultReturnFormat();
+  }
+
+  /**
+   * Method used to construct the base query object used by other objects in this class.
+   *
+   * @param string $phoneFormatType A string value of phone format type.
+   * @return agDoctrineQuery An extended doctrine query object.
+   */
+  protected function _getPhoneFormatType($phoneFormatType)
+  {
+    // construct our base query object
+    $q = agDoctrineQuery::create()
+      ->select('pf.id')
+        ->from('agPhoneFormat pf')
+            ->innerJoin('pf.agPhoneFormatType pft')
+        ->where('pft.phone_format_type = ?', $phoneFormatType);
+
+    return $q;
+  }
+
+  /**
+   * Protected method to set the default phone format used for returning phone.
+   */
+  protected function _setDefaultReturnFormat()
+  {
+    $phoneFormatType = agGlobal::getParam($this->$_globalDefaultPhoneFormatType);
+    $q = $this->_getPhoneFormatType($phoneFormatType);
+    $formatId = $q->execute(array(), Doctrine_Core::HYDRATE_SINGLE_SCALAR);
+
+    $this->setReturnFormat($formatId);
+  }
+
+  /**
+   * A simple method to set the current return format and force the re-generation of the
+   * formatting components.
+   * @param integer $formatId The phone_format_id to set.
+   */
+  public function setReturnFormat($formatId)
+  {
+    $this->_returnFormatId = $formatId;
+    $this->_setPhoneFormatComponent();
+  }
+
+  /**
+   * This method queries the database for the required formatting components and loads several
+   * properties with quick, referenceable information used in formatting endeavors.
+   */
+  protected function _setPhoneFormatComponent()
+  {
+    $q = agDoctrineQuery::create()
+      ->select('pf.id')
+          ->addSelect('pft.validation')
+          ->addSelect('pft.match_pattern')
+          ->addSelect('pft.replacement_pattern')
+        ->from('agPhoneFormat pf')
+            ->innerJoin('pf.agPhoneFormatType pft')
+          ->whereIn('pf.id', $this->_returnFormatId);
+
+    // here we choose a custom hydration method to allow us to manipulate the results data twice
+    $formatComponents = $q->execute(array(), DOCTRINE_CORE::HYDRATE_NONE);
+    foreach($formatComponents as $fc)
+    {
+      // create a simple phone validation array.
+      $this->_phoneValidation[$fc[0]] = $fc[1];
+
+      // create a simple display format array.
+      $this->_phoneDisplayFormat[$fc[0]] = array($fc[2], $fc[3]);
+    }
+  }
+
+  /**
+   * Method used to construct the base query object used by other objects in this class.
+   *
+   * @param array $phoneIds A single-dimension array of phone id's.
+   * @return agDoctrineQuery An extended doctrine query object.
+   */
+  protected function _getPhone($phoneIds)
+  {
+    // if no (null) ID's are passed, get the phoneId's from the class property
+    $phoneIds = $this->getRecordIds($phoneIds);
+
+    // construct our base query object
+    $q = agDoctrineQuery::create()
+      ->select('pc.id')
+          ->addSelect('pc.phone_contact')
+          ->addSelect('pft.match_pattern')
+          ->addSelect('pft.replacement_pattern')
+        ->from('agPhoneContact pc')
+          ->innerJoin('pc.agPhoneFormat pf')
+          ->innerJoin('pf.agPhoneFormatType pft')
+        ->whereIn('pc.id', $phoneIds);
+
+    return $q;
+  }
+
+  /**
+   * A workhorse method useful for returning phone value and it's format id.
+   *
+   * @param array $phoneIds An optional single-dimension array of phone id's.
+   * If NULL, the classes' $phoneIds property is used.
+   * @return array An associative array,
+   * array( phone_id => array(phone, match_pattern, replacement_pattern)).
+   */
+  public function getPhone( $phoneIds = NULL)
+  {
+    // return our base query object
+    $q = $this->_getPhone($phoneIds);
+
+    $results = $q->execute(array(), 'key_value_array');
+    return $results;
+  }
+
+  /**
+   * Method to return a formatted phone as a string.
+   * 
+   * @param array $phoneIds An optional single-dimension array of phone id's. If NULL, the
+   * classes' $phoneIds property is used.
+   * @return array A mono-dimensional associative array keyed by phone_id with the formatted
+   * phone as the value.
+   */
+  public function getFormattedPhone( $phoneIds = NULL )
+  {
+    // always a good idea to explicitly declare this
+    $results = array();
+
+    // now we grab all of our phones.
+    $phones = $this->getPhone($phoneIds);
+
+    // start by iterating over the individual phones
+    foreach ($phones as $phoneId => $phone)
+    {
+      // format phone
+      $formattedPhone = preg_replace($phone[1],
+                                     $phone[2],
+                                     $phone[0]);
+
+      // add formatted phone to our results array
+      $results[$phoneId] = $formattedPhone;
+    }
+
+    return $results;
+  }
+
+  /**
+   * Simple method to return the current phone format id.
+   *
+   * @return integer phone_format_id
+   */
+  public function getFormatId()
+  {
+    return $this->_returnFormatId;
+  }
+
+  /**
+   * A simple getter to return the current phone validation.
+   *
+   * @return array $result An associative array (phone_format_id => validation).
+   */
+  public function getPhoneValidation()
+  {
+    return $this->_phoneValidation;
+  }
+
+  /**
+   * A simple getter to return the current phone format display.
+   *
+   * @return array $result A two dimensional array (phone_format_id => array(match_pattern string, replacement_pattern string)).
+   */
+  public function getPhoneValidation()
+  {
+    return $this->_phoneDisplayFormat;
+  }
+
+}
