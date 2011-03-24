5ecd2733540a64f15fa5b2083d085623bd5ebf16
Nils Stolpe <nils.stolpe@mail.cuny.edu>
2011-03-24 01:15:05 -0400

    Forgot this one.

--- web/js/agModal.js
+++ web/js/agModal.js
@@ -1,24 +1,3 @@
-/**
-* This function is used to check or uncheck a series of checkboxes.
-**/
-$(document).ready(function(){
-  // Checking the checkbox w/ id checkAll will check all boxes w/ class chekToggle
-  // unchecking checkAll will uncheck all checkToggles.
-  $("#checkall").live('click', function () {
-    $('.checkToggle').attr('checked', this.checked);
-  });
-  // This unsets the check in checkAll if one of the checkToggles are unchecked.
-  // or it will set the check on checkAll if all the checkToggles have been checked
-  // individually.
-  $(".checkToggle").live('click', function(){
-    if($(".checkToggle").length == $(".checkToggle:checked").length) {
-      $("#checkall").attr("checked", "checked");
-    } else {
-      $("#checkall").removeAttr("checked");
-    }
-  });
-});
-
 $(document).ready(function() {
   var $dialog = $('<div id="modalContent"></div>')
     .dialog({
@@ -41,44 +20,48 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $('.modalSubmit').live('click',function(){
+    var ble = $(this).parent().attr('name') + ' :input';
+    var blere = '#' + $(this).parent().attr('id') + ' :input';
     $.ajax({
      url: $(this).parent().attr('action'),
      type: "POST",
-     data: $('#' + $(this).parent().attr('name') + ' :input'),
+     data: $('#' + $(this).parent().attr('id') + ' :input'),
      complete:
        function(data) {
+         var boo = data.responseText;
          $('#modalContent').append('<h2 class="overlay">Status Changed</h2>');
          $('.overlay').fadeIn(1200, function() {
            $('.overlay').fadeOut(1200, function() {
              $('.overlay').remove();
+//             pattern = /event\/[a-zA-Z_0-9\+\%\-]*\/facilitygroups/;
+             pattern = /event\/[a-zA-Z_0-9\+\%\-]*\/facilityresource\/[a-zA-Z_0-9\+\%\-]*/;
+             var theer = pattern.exec(data.responseText);
+             if(data.responseText == pattern.exec(data.responseText)) {
+               var $fgroupDialog = $('<div id="#modalFgroup"></div>')
+                 .dialog({
+                   autoOpen: false,
+                   resizable: false,
+                   width: 'auto',
+                   height: 'auto',
+                   draggable: false,
+                   modal: true
+               });
+               $fgroupDialog.dialog("option", "title", "yeah");
+//               Get the name in there somehow.
+               $.post('facilityresource/FF11EC', function(data) {
+                 $fgroupDialog.html(data);
+                 $fgroupDialog.dialog('open');
+               });
+             }
              $('#modalContent').load($(this).parent().attr('action'));
            })
          })
-         pattern = /event\/[a-zA-Z_0-9\+\%\-]*\/fgroup/;
-         if(data.responseText == pattern.exec(textString)) {
-//         if(textString == result) {
-           var $fgroupDialog = $('<div id="#modalFgroup"></div>')
-             .dialog({
-               autoOpen: false,
-               resizable: false,
-               width: 'auto',
-               height: 'auto',
-               draggable: false,
-               modal: true
-           });
-           $fgroupDialog.dialog("option", "title", "yeah");
-           $.post('fgroup', function(data) {
-             $fgroupDialog.html(data);
-             $fgroupDialog.dialog('open');
-           })
-         }
        }
-    })
-    
+    });
+
     return false;
   });
    $('#modalContent').bind('dialogclose', function() {
      $('#tableContainer').load(window.location.pathname + ' .singleTable');
    });
 });
-
