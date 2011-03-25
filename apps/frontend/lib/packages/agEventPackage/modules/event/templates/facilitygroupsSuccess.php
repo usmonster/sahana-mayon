75d094e62e258eca36ad9cdc949d06ac4a7e4a42
Nils Stolpe <nils.stolpe@mail.cuny.edu>
2011-03-25 15:16:48 -0400

    Added the 'n' back to 'Event' in facilitygroupsSuccess.php

--- apps/frontend/lib/packages/agEventPackage/modules/event/templates/facilitygroupsSuccess.php
+++ apps/frontend/lib/packages/agEventPackage/modules/event/templates/facilitygroupsSuccess.php
@@ -1,5 +1,5 @@
 
-<h2><span class="highlightedText"><?php echo $event_name ?></span> Evet Facility Group and Facility Management</h2>
+<h2><span class="highlightedText"><?php echo $event_name ?></span> Event Facility Group and Facility Management</h2>
 <?php use_javascript('agMain.js'); ?>
 <?php include_partial('fgroupForm', array('fgroupForm' => $fgroupForm, 'event_name' => $event_name, 'facilitygroupsForm' => $facilitygroupsForm)) ?>
 
