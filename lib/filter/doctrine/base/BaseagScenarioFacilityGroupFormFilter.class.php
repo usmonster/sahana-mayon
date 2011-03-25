ac66ae4aef4ff8312286d0ea60ad03f549163305
Shirley Chan <shirley.chan@mail.cuny.edu>
2011-03-25 18:07:10 -0400

    Reverting agFacilityResource table back to its original state where there's no facility_resource_code.

--- lib/filter/doctrine/base/BaseagScenarioFacilityGroupFormFilter.class.php
+++ lib/filter/doctrine/base/BaseagScenarioFacilityGroupFormFilter.class.php
@@ -18,6 +18,8 @@ abstract class BaseagScenarioFacilityGroupFormFilter extends BaseFormFilterDoctr
       'facility_group_type_id'              => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('agFacilityGroupType'), 'add_empty' => true)),
       'facility_group_allocation_status_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('agFacilityGroupAllocationStatus'), 'add_empty' => true)),
       'activation_sequence'                 => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'created_at'                          => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'updated_at'                          => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
       'ag_facility_resource_list'           => new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => 'agFacilityResource')),
     ));
 
@@ -27,6 +29,8 @@ abstract class BaseagScenarioFacilityGroupFormFilter extends BaseFormFilterDoctr
       'facility_group_type_id'              => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('agFacilityGroupType'), 'column' => 'id')),
       'facility_group_allocation_status_id' => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('agFacilityGroupAllocationStatus'), 'column' => 'id')),
       'activation_sequence'                 => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
+      'created_at'                          => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'updated_at'                          => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
       'ag_facility_resource_list'           => new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => 'agFacilityResource', 'required' => false)),
     ));
 
@@ -71,6 +75,8 @@ abstract class BaseagScenarioFacilityGroupFormFilter extends BaseFormFilterDoctr
       'facility_group_type_id'              => 'ForeignKey',
       'facility_group_allocation_status_id' => 'ForeignKey',
       'activation_sequence'                 => 'Number',
+      'created_at'                          => 'Date',
+      'updated_at'                          => 'Date',
       'ag_facility_resource_list'           => 'ManyKey',
     );
   }
--- lib/form/doctrine/base/BaseagFacilityResourceForm.class.php
+++ lib/form/doctrine/base/BaseagFacilityResourceForm.class.php
@@ -16,7 +16,6 @@ abstract class BaseagFacilityResourceForm extends BaseFormDoctrine
   {
     $this->setWidgets(array(
       'id'                              => new sfWidgetFormInputHidden(),
-      'facility_resource_code'          => new sfWidgetFormInputText(),
       'facility_id'                     => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('agFacility'), 'add_empty' => false)),
       'facility_resource_type_id'       => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('agFacilityResourceType'), 'add_empty' => false)),
       'facility_resource_status_id'     => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('agFacilityResourceStatus'), 'add_empty' => false)),
@@ -29,7 +28,6 @@ abstract class BaseagFacilityResourceForm extends BaseFormDoctrine
 
     $this->setValidators(array(
       'id'                              => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
-      'facility_resource_code'          => new sfValidatorString(array('max_length' => 10)),
       'facility_id'                     => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('agFacility'))),
       'facility_resource_type_id'       => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('agFacilityResourceType'))),
       'facility_resource_status_id'     => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('agFacilityResourceStatus'))),
@@ -41,10 +39,7 @@ abstract class BaseagFacilityResourceForm extends BaseFormDoctrine
     ));
 
     $this->validatorSchema->setPostValidator(
-      new sfValidatorAnd(array(
-        new sfValidatorDoctrineUnique(array('model' => 'agFacilityResource', 'column' => array('facility_id', 'facility_resource_type_id'))),
-        new sfValidatorDoctrineUnique(array('model' => 'agFacilityResource', 'column' => array('facility_resource_code'))),
-      ))
+      new sfValidatorDoctrineUnique(array('model' => 'agFacilityResource', 'column' => array('facility_id', 'facility_resource_type_id')))
     );
 
     $this->widgetSchema->setNameFormat('ag_facility_resource[%s]');
