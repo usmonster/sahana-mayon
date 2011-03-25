221360b3829619014aa4acd4c673dcdb7c9c9b10
Darlene E McCullough <darlene.mccullough@mail.cuny.edu>
2011-03-25 15:48:48 -0400

    wiki pages and tooltips

--- apps/frontend/modules/scenario/templates/reviewSuccess.php
+++ apps/frontend/modules/scenario/templates/reviewSuccess.php
@@ -3,12 +3,18 @@
 <h3><?php echo $scenario_description ?></h3>
 <!-- ideally the above should be 'editable text', i.e. when clicked on they convert to input fields -->
 <br /><hr />
-<a href="<?php echo url_for('scenario/edit?id=' . $scenario_id) ?>" class="linkButton">Modify Scenario Meta Information</a> <br /><br />
-<a href="<?php echo url_for('scenario/listgroups?id=' . $scenario_id) ?>" class="linkButton">Manage Facilities/Groups</a> <br /><br />
-<a href="<?php echo url_for('scenario/staffpool?id=' . $scenario_id) ?>" class="linkButton">Staff Pool Definitions</a> <br /><br />
-<a href="<?php echo url_for('scenario/shifttemplates?id=' . $scenario_id) ?>" class="linkButton">Shift Templates</a> <br /><br />
-<a href="<?php echo url_for('scenario/shifts?id=' . $scenario_id) ?>" class="linkButton">Scenario Shifts</a> <br /><br />
-<a href="<?php echo url_for('scenario/staffresources?id=' . $scenario_id) ?>" class="linkButton">Staff Resource Requirements</a> <br /><br />
+<a href="<?php echo url_for('scenario/edit?id=' . $scenario_id) ?>" 
+   title="Modify Basic Scenario Information" class="linkButton">Scenario Name and Description</a> <br /><br />
+<a href="<?php echo url_for('scenario/listgroups?id=' . $scenario_id) ?>" 
+   title="Edit Facility Groups" class="linkButton">Manage Facility Groups</a> <br /><br />
+<a href="<?php echo url_for('scenario/staffresources?id=' . $scenario_id) ?>"
+   title="Edit Staff Requirements" class="linkButton">Staff Resource Requirements</a> <br /><br />
+<a href="<?php echo url_for('scenario/staffpool?id=' . $scenario_id) ?>" 
+   title="Edit Staff Pool" class="linkButton">Staff Pool Definitions</a> <br /><br />
+<a href="<?php echo url_for('scenario/shifttemplates?id=' . $scenario_id) ?>"
+   title="Edit Shift Templates" class="linkButton">Shift Templates</a> <br /><br />
+<a href="<?php echo url_for('scenario/shifts?id=' . $scenario_id) ?>"
+   title="View Scenario Shifts" class="linkButton">Scenario Shifts</a> <br /><br />
 
 
 <form action="<?php echo url_for('event/meta')?>" method="post" name="scenario">
