4f0701b1d5df1e7f2e7d64c3a5c2c7c343a0b61b
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-03-29 11:39:55 -0400

    Unnecessary parameter in the url on search button click fix (needs Charles approval)

--- apps/frontend/modules/search/templates/_searchForm.php
+++ apps/frontend/modules/search/templates/_searchForm.php
@@ -4,5 +4,5 @@
          id="search_keywords" class="searchForm"/>
   <input type="submit" value="Search" class="buttonWhite" />-->
   <input type="text" name="query" value="<?php echo $sf_request->getParameter('query'); ?>"
-         id="search_keywords" class="searchTextBox"/><input class="searchButton" type="submit" name="submit" value="" style="margin-top: 3px; width: 28px; height:23px; display:inline; border: none;">
+         id="search_keywords" class="searchTextBox"/><input class="searchButton" type="submit" style="margin-top: 3px; width: 28px; height:23px; display:inline; border: none; font-size: 0px">
 </form>
