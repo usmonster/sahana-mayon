bffd1863b862b8fc19d0a398527f032ce13671ea
Usman Akeju <usman.akeju@mail.cuny.edu>
2011-03-29 15:44:29 -0400

    - adds id to search button

--- apps/frontend/modules/search/templates/_searchForm.php
+++ apps/frontend/modules/search/templates/_searchForm.php
@@ -4,5 +4,7 @@
          id="search_keywords" class="searchForm"/>
   <input type="submit" value="Search" class="buttonWhite" />-->
   <input type="text" name="query" value="<?php echo $sf_request->getParameter('query'); ?>"
-         id="search_keywords" class="searchTextBox"/><input class="searchButton" type="submit" style="margin-top: 3px; width: 28px; height:23px; display:inline; border: none; font-size: 0px">
+         id="search_keywords" class="searchTextBox"/>
+  <input id="submit_search" class="searchButton" type="submit"
+         style="margin-top: 3px; width: 28px; height:23px; display:inline; border: none; font-size: 0px" />
 </form>
