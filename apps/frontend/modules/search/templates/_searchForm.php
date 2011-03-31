bb8c873795acaea0831618baf0fc7386ef16a9d7
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-03-31 15:55:29 -0400

    Commented out JQUERY SEARCH BAR Update
    Edit view.yml to include jquery.tipsy.js and uncomment code in _searchForm.php to enable Jquery Search bar.

--- apps/frontend/modules/search/templates/_searchForm.php
+++ apps/frontend/modules/search/templates/_searchForm.php
@@ -1,10 +1,48 @@
 <?php use_helper('I18N') ?>
+
+
 <form action="<?php echo url_for($sf_request->getParameter('module') . '/search') ?>" method="get" class="displayInline">
-  <!--<input type="text" name="query" value="<?php echo $sf_request->getParameter('query'); ?>"
-         id="search_keywords" class="searchForm"/>
-  <input type="submit" value="Search" class="buttonWhite" />-->
   <input type="text" name="query" value="<?php echo $sf_request->getParameter('query'); ?>"
-         id="search_keywords" class="searchTextBox"/>
-  <input id="submit_search" class="searchButton" type="submit"
+         id="search_keywords" class="searchTextBox"/><input id="submit_search" class="searchButton" type="submit"
          style="margin-top: 3px; width: 28px; height:23px; display:inline; border: none; font-size: 0px" />
+</form> 
+
+<!--Code for JQUERY Search Box
+
+<a href="login" class="signin" style="float:right; margin-top: 12px; margin-right:12px; text-decoration:none; color:#999"><span>Search</span></a>
+          <span class="MarginPointOneEm">
+           <fieldset id="signin_menu">
+          <form action="<?php echo url_for($sf_request->getParameter('module') . '/search') ?>" method="get" class="displayInline">
+
+  <input type="text" name="query" value="<?php echo $sf_request->getParameter('query'); ?>"
+         id="search_keywords" class="searchTextBox"/><input id="submit_search" class="searchButton" type="submit"
+         style="border: none; font-size: 0px" />
 </form>
+ </fieldset>
+        </span>
+
+<script type="text/javascript">
+        $(document).ready(function() {
+
+            $(".signin").click(function(e) {
+				e.preventDefault();
+                $("fieldset#signin_menu").toggle();
+				$(".signin").toggleClass("menu-open");
+				$("#search_keywords").focus();
+            });
+
+			$("fieldset#signin_menu").mouseup(function() {
+				return false
+			});
+			$(document).click(function(e) {
+				if($(e.target).parent("a.signin").length==0) {
+					$(".signin").removeClass("menu-open");
+					$("fieldset#signin_menu").hide();
+					$("#search_keywords").val("");
+				}
+			});
+
+        });
+    </script>
+
+ End Code for JQUERY Search Box-->
