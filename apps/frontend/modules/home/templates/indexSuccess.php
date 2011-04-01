c032a65e2f00cb4c5f97b0c88515433ee14866ab
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-04-01 14:32:15 -0400

    Included shortcut links on homepage

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -18,3 +18,18 @@ if ($sf_user->isAuthenticated()) {
   information and facilities through an easy to use web interface. </p>
 
 <p>To begin, select an option from the menus above.</p>
+
+<?php
+if ($sf_user->isAuthenticated()) {
+  echo "<table cellspacing='20'>
+    <tr>
+      <td><a href='./prepare' class='linkButton width140'>Prepare</a></td>
+      <td><a href='./scenario' class='linkButton width140'>Respond</a></td>
+    </tr>
+    <tr>
+      <td><a href='./wiki' class='linkButton width140'>Wiki Home</a></td>
+      <td><a href='./admin' class='linkButton width140'>Administration</a></td>
+    </tr>
+</table>";
+}
+?>
