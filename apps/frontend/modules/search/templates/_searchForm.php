67f74ca1fb93e0436627f9b9e865db203b70463b
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-04-01 12:16:20 -0400

    Minor tweak to Jquery Search box (code has been commented out)

--- apps/frontend/modules/search/templates/_searchForm.php
+++ apps/frontend/modules/search/templates/_searchForm.php
@@ -7,9 +7,9 @@
          style="margin-top: 3px; width: 28px; height:23px; display:inline; border: none; font-size: 0px" />
 </form> 
 
-<!--Code for JQUERY Search Box
 
-<a href="login" class="signin" style="float:right; margin-top: 12px; margin-right:12px; text-decoration:none; color:#999"><span>Search</span></a>
+<!--Code for JQUERY Search Box
+<a href="#" class="signin" style="float:right; margin-top: 12px; margin-right:12px; text-decoration:none; color:#999"><span>Search</span></a>
           <span class="MarginPointOneEm">
            <fieldset id="signin_menu">
           <form action="<?php echo url_for($sf_request->getParameter('module') . '/search') ?>" method="get" class="displayInline">
@@ -29,20 +29,26 @@
                 $("fieldset#signin_menu").toggle();
 				$(".signin").toggleClass("menu-open");
 				$("#search_keywords").focus();
+                                $("#search_keywords").select();
             });
 
 			$("fieldset#signin_menu").mouseup(function() {
 				return false
 			});
-			$(document).click(function(e) {
+			
+                        $('fieldset#signin_menu').click(function(event) {
+				event.stopPropagation();
+			});
+
+                        $(document).click(function(e) {
 				if($(e.target).parent("a.signin").length==0) {
 					$(".signin").removeClass("menu-open");
 					$("fieldset#signin_menu").hide();
-					$("#search_keywords").val("");
+                                        //$("fieldset#signin_menu").fadeOut(1000);
+					//$("#search_keywords").val("");
 				}
 			});
 
         });
     </script>
-
  End Code for JQUERY Search Box-->
