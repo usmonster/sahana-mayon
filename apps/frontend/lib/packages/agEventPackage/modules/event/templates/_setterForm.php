23511c447082acb1b4cadd21fc5e170154a51be7
Nils Stolpe <nils.stolpe@mail.cuny.edu>
2011-04-18 17:29:34 -0400

    Removed an unused variable from _setterForm.php

--- apps/frontend/lib/packages/agEventPackage/modules/event/templates/_setterForm.php
+++ apps/frontend/lib/packages/agEventPackage/modules/event/templates/_setterForm.php
@@ -1,4 +1,4 @@
-<form action="<?php echo url_for($url . $urlParam); ?>" id="<?php echo $id; ?>" method="post">
+<form action="<?php echo url_for($url); ?>" id="<?php echo $id; ?>" method="post">
   <?php echo $form; ?>
 </form>
 
