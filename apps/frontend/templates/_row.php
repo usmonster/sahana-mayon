5c857e29fa8f0b65bac873c96566f446c42cf509
Usman Akeju <usman.akeju@mail.cuny.edu>
2011-04-28 11:46:52 -0400

    - removes outer spaces from list item table data

--- apps/frontend/templates/_row.php
+++ apps/frontend/templates/_row.php
@@ -1,35 +1,28 @@
 <?php
-$data = $obj;//->getRaw();
+$data = $obj; //->getRaw();
 //$column_array = $displayColumns->toArray();
-
 //this is used draw each result as a row, coming from _list.php
 //$columns are our desired columns.. keyed by the name of the value array to echo
-foreach($displayColumns as $key => $value)
-{
+foreach ($displayColumns as $key => $value) {
   $display_columns[] = $key;
 }
 ?>
 <tr>
 
-<?php foreach ($data as $column => $value) {
-
+  <?php
+  foreach ($data as $column => $value) {
 //array_keys
+    if (in_array($column, $display_columns)) {
   ?>
-    <?php
-    if(in_array($column, $display_columns)){
-      ?>
-    <td>
-<?php
-
-if($column == 'fn')
-{
-  echo '<a class="linkButton" href="' . url_for($target_module . '/edit?id=' . $data['id']) . '">' . $value . '</a>';
-}else{
-  echo $value;
-}
-?>
-      </td>
-      <?php } ?>
-<?php }
-?>
+      <td><?php
+      if ($column == 'fn') {
+        echo link_to($value, $target_module . '/edit?id=' . $data['id'], array('class' => 'linkButton', 'title' => 'Create New Facility'));
+      } else {
+        echo $value;
+      }
+  ?></td>
+  <?php
+    }
+  }
+  ?>
 </tr>
