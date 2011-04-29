2d73c17478ff060ef85e621f2098d204123817f9
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-04-29 10:48:04 -0400

    Scenario review page layout update

--- apps/frontend/modules/scenario/templates/reviewSuccess.php
+++ apps/frontend/modules/scenario/templates/reviewSuccess.php
@@ -2,29 +2,58 @@
 
 <h3><?php echo $scenario_description ?></h3>
 <!-- ideally the above should be 'editable text', i.e. when clicked on they convert to input fields -->
-<br /><hr />
-<a href="<?php echo url_for('scenario/meta?id=' . $scenario_id) ?>"
-   title="Modify Basic Scenario Information" class="linkButton">Scenario Name and Description</a> <br /><br />
-<a href="<?php echo url_for('scenario/resourcetypes?id=' . $scenario_id) ?>"
-   title="Edit Required Resource Types for Scenario" class="linkButton">Manage Required Resource Types</a> <br /><br />
-<a href="<?php echo url_for('scenario/listgroup?id=' . $scenario_id) ?>" 
-   title="Edit Facility Groups" class="linkButton">Manage Facility Groups</a> <br /><br />
-<a href="<?php echo url_for('scenario/staffresources?id=' . $scenario_id) ?>"
-   title="Edit Staff Requirements" class="linkButton">Staff Resource Requirements</a> <br /><br />
-<a href="<?php echo url_for('scenario/staffpool?id=' . $scenario_id) ?>" 
-   title="Edit Staff Pool" class="linkButton">Staff Pool Definitions</a> <br /><br />
-<a href="<?php echo url_for('scenario/shifttemplates?id=' . $scenario_id) ?>"
-   title="Edit Shift Templates" class="linkButton">Shift Templates</a> <br /><br />
-<a href="<?php echo url_for('scenario/shifts?id=' . $scenario_id) ?>"
-   title="View Scenario Shifts" class="linkButton">Scenario Shifts</a> <br /><br />
+<br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vestibulum ante vitae felis aliquet eget condimentum felis sollicitudin. Curabitur eu semper ligula. Sed odio elit, eleifend eu sodales nec, laoreet non lacus. Suspendisse sit amet magna elit. Donec ullamcorper vestibulum rutrum. Fusce faucibus ipsum ut diam placerat mattis. Vestibulum id est orci, scelerisque gravida lacus. Curabitur venenatis ullamcorper congue. Fusce molestie dui turpis, in sagittis sapien. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <br> <br />
+<table class="reviewTable">
+    <tr class="head">
+        <th class="row1">Steps</th>
+        <th>Description</th>
+    </tr>
+    <tr>
+        <td><a href="<?php echo url_for('scenario/meta?id=' . $scenario_id) ?>"
+   title="Modify Basic Scenario Information">Scenario Name and Description</a></td>
+        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vestibulum ante vitae felis aliquet eget condimentum felis sollicitudin.</td>
+    </tr>
+    <tr>
+        <td><a href="<?php echo url_for('scenario/resourcetypes?id=' . $scenario_id) ?>"
+   title="Edit Required Resource Types for Scenario">Manage Required Resource Types</a></td>
+        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vestibulum ante vitae felis aliquet eget condimentum felis sollicitudin.</td>
+    </tr>
+    <tr>
+        <td><a href="<?php echo url_for('scenario/listgroup?id=' . $scenario_id) ?>"
+   title="Edit Facility Groups">Manage Facility Groups</a></td>
+        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vestibulum ante vitae felis aliquet eget condimentum felis sollicitudin.</td>
+    </tr>
+    <tr>
+        <td><a href="<?php echo url_for('scenario/staffresources?id=' . $scenario_id) ?>"
+   title="Edit Staff Requirements">Staff Resource Requirements</a></td>
+        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vestibulum ante vitae felis aliquet eget condimentum felis sollicitudin.</td>
+    </tr>
+    <tr>
+        <td><a href="<?php echo url_for('scenario/staffpool?id=' . $scenario_id) ?>"
+   title="Edit Staff Pool">Staff Pool Definitions</a></td>
+        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vestibulum ante vitae felis aliquet eget condimentum felis sollicitudin.</td>
+    </tr>
+    <tr>
+        <td><a href="<?php echo url_for('scenario/shifttemplates?id=' . $scenario_id) ?>"
+   title="Edit Shift Templates">Shift Templates</a></td>
+        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vestibulum ante vitae felis aliquet eget condimentum felis sollicitudin.</td>
+    </tr>
+    <tr>
+        <td><a href="<?php echo url_for('scenario/shifts?id=' . $scenario_id) ?>"
+   title="View Scenario Shifts">Scenario Shifts</a></td>
+        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vestibulum ante vitae felis aliquet eget condimentum felis sollicitudin.</td>
+    </tr>
+</table>
 
+
+ <br /> 
 
 <form action="<?php echo url_for('event/meta')?>" method="post" name="scenario">
   <input type="hidden" value="<?php echo $scenario_id ?>" id ="ag_scenario_list" name="ag_scenario_list" />
-  <input type="submit" value="Deploy Scenario as Event" class="linkButton" />
+  <input type="submit" value="Deploy Scenario as Event"  class="linkButton" />
 </form>
 
-
+<br />
   <hr />
 
   <a href="<?php echo url_for('scenario/list') ?>" class="linkButton">List Scenarios</a>
