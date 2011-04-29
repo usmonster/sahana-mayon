9bee482a47b075f1c62748b9995866fe94cb076f
Nils Stolpe <nils.stolpe@mail.cuny.edu>
2011-04-29 16:56:32 -0400

    Small jQuery change.

--- apps/frontend/modules/scenario/templates/resourcetypesSuccess.php
+++ apps/frontend/modules/scenario/templates/resourcetypesSuccess.php
@@ -5,10 +5,15 @@
 ?>
 <br />
 <script language="Javascript" type="text/javascript"> 
-    $(document).ready(function(){var maxHeight=0;
-        $('div.inlineListWrapper')
-        .each(function(){maxHeight = Math.max(maxHeight, $(this).height());});
-        $('div.inlineLists').height(maxHeight);})
+    $(document).ready(function(){
+        var maxHeight=0;
+
+        $('div.inlineLists').each(function(){
+          maxHeight = Math.max(maxHeight, $(this).height());
+        });
+
+       $('div.inlineLists').height(maxHeight);
+    })
 
 </script>
 <h2>Edit Required Resource Types for Scenario</h2> <br />
