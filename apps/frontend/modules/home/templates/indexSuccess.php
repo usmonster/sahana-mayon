5a939231c53e090b42c6be820875b9410e4a03bb
Darlene E McCullough <darlene.mccullough@mail.cuny.edu>
2011-05-02 11:01:56 -0400

    cleaned up home text and fixed instructions outside of auth

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -2,12 +2,6 @@
 <h2>New York City Sahana Agasti</h2>
 
 <?php
-if (!$sf_user->isAuthenticated()) {
-  echo "<h2> To begin, please login in the upper right.</h2>";
-}
-?>
-<br>
-<?php
 if ($sf_user->isAuthenticated()) {
   echo "<h3> Welcome to Sahana Agasti Emergency Preparedness and Response Software</h3>";
 }
@@ -17,11 +11,19 @@ if ($sf_user->isAuthenticated()) {
   Agasti is an emergency management application with tools to manage staff, resources, client
   information and facilities through an easy to use web interface. </p>
 
-<h3>To begin, select an option from the menus above or the icons below.</h3>
+<?php
+if (!$sf_user->isAuthenticated()) {
+  echo "<h2> To begin, please login in the upper right.</h2>";
+}
+?>
 
 <?php
 if ($sf_user->isAuthenticated()) {
-?><table cellspacing='20'>
+?>
+  <h3>To begin, select an option from the menus above or the icons below.</h3>  
+  
+  
+  <table cellspacing='20'>
     <tr>
       <td><?php echo link_to('Prepare', 'home/prepare', array('class' => 'linkButton width140', 'title' => 'Prepare')); ?></td>
       <td><?php echo link_to('Respond', 'home/respond', array('class' => 'linkButton width140', 'title' => 'Respond')); ?></td>
