aa1d58ab07dd3b813292e49d600a99b61e18827e
Nils Stolpe <nils.stolpe@mail.cuny.edu>
2011-05-03 10:33:38 -0400

    Styling and layout changes to scenario fgroup.

--- apps/frontend/modules/scenario/templates/_groupform.php
+++ apps/frontend/modules/scenario/templates/_groupform.php
@@ -120,141 +120,104 @@ use_javascript('tooltip.js'); ?>
 </script>
 <noscript>in order to set the activation sequence of resource facilities and add them to the
   facility group, you will need javascript enabled</noscript>
-<form name="faciliy_group_form" id="facility_group_form" action="<?php
+
+
+
+<p>Use the checkboxes below to determine which facility resource types are display in the Available list.</p>
+<div class="bucketHolder" >
+  <form name="faciliy_group_form" id="facility_group_form" action="<?php
 echo url_for
     ('scenario/fgroup?id=' . $scenario_id) . (!$groupform->getObject()->isNew() ? '?groupid=' . $groupform->getObject()->getId() : '') ?>
       " method="post" <?php $groupform->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
-
-  <div>
-    <?php if (!$groupform->getObject()->isNew()): ?>
-<!--      <input class="linkButton" type="submit" value="Delete" name="Delete"/>-->
-<!--      <input class="linkButton" type="submit" value="Save" id="selecter" onclick="serialTran()"/>-->
-    <?php endif; ?>
-    <?php echo $groupform; ?>
-  </div>
-  <p>Use the checkboxes below to determine which facility resource types are display in the Available list.</p>
-  <div class="bucketHolder" >
+    <div>
+      <?php echo $groupform; ?>
+    </div>
     <br />
     <div class="testTableContainer ">
-    <table class="testTable available" cellspacing="0">
-      <thead>
-        <caption><a href="#" id="revealer" title="Facility Resource Type Filter">&#9654;</a>Available Facility Resources</caption>
-      </thead>
-      <tbody>
-        <tr>
-          <th class="left">Facility Code</th>
-          <th>Resource Type</th>
-        </tr>
-      <?php foreach ($availableFacilityResources as $availableFacilityResource): ?>
-        <tr id="facility_resource_id_<?php echo $availableFacilityResource['fr_id']; ?>" class="sort facility_resource_type_<?php echo $availableFacilityResource['frt_id']; ?>">
-          <td class="left" title="<?php echo $availableFacilityResource['f_facility_code'];?>">
-            <?php echo $availableFacilityResource['f_facility_name'] ?>
-          </td>
-          <td class="right" title="<?php echo $availableFacilityResource['frt_facility_resource_type_abbr'] ?>">
-            <?php echo $availableFacilityResource['frt_facility_resource_type'] ?>
-          </td>
-        </tr>
-      <?php endforeach; ?>
-      </tbody>
-    </table>
+      <table class="testTable available" cellspacing="0">
+        <thead>
+          <caption><a href="#" id="revealer" title="Facility Resource Type Filter">&#9654;</a>Available Facility Resources</caption>
+        </thead>
+        <tbody>
+          <tr>
+            <th class="left">Facility Code</th>
+            <th>Resource Type</th>
+          </tr>
+        <?php foreach ($availableFacilityResources as $availableFacilityResource): ?>
+          <tr id="facility_resource_id_<?php echo $availableFacilityResource['fr_id']; ?>" class="sort facility_resource_type_<?php echo $availableFacilityResource['frt_id']; ?>">
+            <td class="left" title="<?php echo $availableFacilityResource['f_facility_code'];?>">
+              <?php echo $availableFacilityResource['f_facility_name'] ?>
+            </td>
+            <td class="right" title="<?php echo $availableFacilityResource['frt_facility_resource_type_abbr'] ?>">
+              <?php echo $availableFacilityResource['frt_facility_resource_type'] ?>
+            </td>
+          </tr>
+        <?php endforeach; ?>
+        </tbody>
+      </table>
     </div>
 
-<div class="testTableContainer">
-  <table class="testTableParent" cellspacing="0">
-    <thead>
-      <caption>Allocated Facility Resources</caption>
-    </thead>
-    <?php foreach($selectStatuses as $selectStatus): ?>
-    <tr class="sortHead" id="<?php echo $selectStatus['fras_facility_resource_allocation_status']; ?>">
-      <th class="left">
-        <a href="#" class="<?php echo $selectStatus['fras_facility_resource_allocation_status']; ?>" title="Collapse">&#9660;</a>
-        <?php echo ucwords($selectStatus['fras_facility_resource_allocation_status']); ?>
-      </th>
-      <th class="count right">
-            Count:
-      </th>
-    </tr>
-    <tr>
-      <td colspan="3" class="container">
-        <div class="<?php echo $selectStatus['fras_facility_resource_allocation_status']; ?>">
-          <table class="testTable allocated" cellspacing="0">
-            <tbody class="<?php echo $selectStatus['fras_facility_resource_allocation_status']; ?>" title="<?php echo $selectStatus['fras_facility_resource_allocation_status']; ?>">
-              <tr>
-                <th class="left">Facility Code</th>
-                <th class="inner">Resource Type</th>
-                <th class="right narrow">Priority</th>
-              </tr>
-            <?php if ($allocatedFacilityResources): ?>
-              <?php foreach ($allocatedFacilityResources[$selectStatus['fras_facility_resource_allocation_status']] as $allocatedFacilityResource): ?>
-              <tr id="facility_resource_id_<?php echo $allocatedFacilityResource['fr_id']; ?>" class="sort serialIn facility_resource_type_<?php echo $allocatedFacilityResource['frt_id']; ?>">
-                <td class="left" title="<?php echo $allocatedFacilityResource['f_facility_code'];?>">
-                  <?php echo $allocatedFacilityResource['f_facility_name'] ?>
-                </td>
-                <td class="inner" title="<?php echo $allocatedFacilityResource['frt_facility_resource_type_abbr'] ?>">
-                  <?php echo $allocatedFacilityResource['frt_facility_resource_type'] ?>
-                </td>
-                <td class="right narrow">
-                  <input type="text" class="inputGraySmall" value="<?php echo $allocatedFacilityResource['sfr_activation_sequence']; ?>">
-                </td>
-              </tr>
-              <?php endforeach; ?>
-            <?php endif; ?>
-            <tbody>
-          </table>
-        </div>
-      </td>
-    </tr>
-    <?php endforeach; ?>
-  </table>
+    <div class="testTableContainer">
+      <table class="testTableParent" cellspacing="0">
+        <thead>
+          <caption>Allocated Facility Resources</caption>
+        </thead>
+        <?php foreach($selectStatuses as $selectStatus): ?>
+        <tr class="sortHead" id="<?php echo $selectStatus['fras_facility_resource_allocation_status']; ?>">
+          <th class="left">
+            <a href="#" class="<?php echo $selectStatus['fras_facility_resource_allocation_status']; ?>" title="Collapse">&#9660;</a>
+            <?php echo ucwords($selectStatus['fras_facility_resource_allocation_status']); ?>
+          </th>
+          <th class="count right">Count:</th>
+        </tr>
+        <tr>
+          <td colspan="3" class="container">
+            <div class="<?php echo $selectStatus['fras_facility_resource_allocation_status']; ?>">
+              <table class="testTable allocated" cellspacing="0">
+                <tbody class="<?php echo $selectStatus['fras_facility_resource_allocation_status']; ?>" title="<?php echo $selectStatus['fras_facility_resource_allocation_status']; ?>">
+                  <tr>
+                    <th class="left">Facility Code</th>
+                    <th class="inner">Resource Type</th>
+                    <th class="right narrow">Priority</th>
+                  </tr>
+                <?php if ($allocatedFacilityResources): ?>
+                  <?php foreach ($allocatedFacilityResources[$selectStatus['fras_facility_resource_allocation_status']] as $allocatedFacilityResource): ?>
+                  <tr id="facility_resource_id_<?php echo $allocatedFacilityResource['fr_id']; ?>" class="sort serialIn facility_resource_type_<?php echo $allocatedFacilityResource['frt_id']; ?>">
+                    <td class="left" title="<?php echo $allocatedFacilityResource['f_facility_code'];?>">
+                      <?php echo $allocatedFacilityResource['f_facility_name'] ?>
+                    </td>
+                    <td class="inner" title="<?php echo $allocatedFacilityResource['frt_facility_resource_type_abbr'] ?>">
+                      <?php echo $allocatedFacilityResource['frt_facility_resource_type'] ?>
+                    </td>
+                    <td class="right narrow">
+                      <input type="text" class="inputGraySmall" value="<?php echo $allocatedFacilityResource['sfr_activation_sequence']; ?>">
+                    </td>
+                  </tr>
+                  <?php endforeach; ?>
+                <?php endif; ?>
+                <tbody>
+              </table>
+            </div>
+          </td>
+        </tr>
+        <?php endforeach; ?>
+      </table>
+    </div>
+    <br />
+    <br />
+    <input class="linkButton" type="submit" value="Save and Create Another" name="Another" onclick="serialTran()"/>
+    <input class="linkButton" type="submit" value="Save and Assign Staff Requirements" name="AssignAll" onclick="serialTran()"/>
+  <?php if ($existingFgroups == true): ?>
+    <a href="<?php echo url_for('scenario/staffresources?id=' . $scenario_id) ?>" class="linkButton">Continue</a>
+  <?php endif; ?>
+  </form>
 </div>
-<!--    <ul id="bavailable" class="bucket">-->
-      <?php
-//      foreach ($availableFacilityResources as $afr) {
-//        echo '<li id="' . $afr['fr_id'] . '" title="' .
-//        $afr['frt_facility_resource_type'] . '">' .
-//        $afr['f_facility_name'] . ': ' .
-//        ucwords($afr['frt_facility_resource_type']) .
-//        '</li>'; //we could set the id here to a set of ids
-//      }
-      ?>
-
-<!--    </ul>-->
-<!--    <ul id="allocated" class="bucket">-->
-      <?php
-//      if ($allocatedFacilityResources) {
-//
-//        foreach ($allocatedFacilityResources as $facility_resource) {
-//          echo '<li id="' . $facility_resource->getId() . '" title="' .
-//          $facility_resource->getAgFacilityResourceType() . '">' .
-//          $facility_resource->getAgFacility()->getFacilityName() . ': ' .
-//          ucwords($facility_resource->getAgFacilityResourceType()->facility_resource_type) .
-//          '</li>'; //we could set the id here to a set of ids
-//          /**
-//           * @todo [$curopt->activation_sequence] needs to still be applied to the list,
-//           */
-//        }
-//      }
-      ?>
-<!--    </ul>-->
-  </div>
-  <br/>
-  <div class="tooltips" >
-    <span id="allocated_tip">
+<br/>
+<div class="tooltips" >
+  <span id="allocated_tip">
 <?php echo "urltowiki/allocated_tooltip"; ?>
-    </span>
-  </div>
-  <br />
-  <input class="linkButton" type="submit" value="Save and Create Another" name="Another" onclick="serialTran()"/>
-  <input class="linkButton" type="submit" value="Save and Assign Staff Requirements" name="AssignAll" onclick="serialTran()"/>
-
-  <?php
-      if ($existingFgroups == true) {
-  ?>
- <a href="<?php echo url_for('scenario/staffresources?id=' . $scenario_id) ?>" class="linkButton">Continue</a>
-  <?php
-      }
-  ?>
-</form>
+  </span>
+</div>
 <?php
   $contents = $sf_data->getRaw('facilityResourceTypes');
   echo buildCheckBoxTable($contents, 'id', 'facility_resource_type', 'checkBoxTable searchParams', 'revealable', 2, 'facility_resource_type_', 'facility_resource_type_abbr', true, true);
