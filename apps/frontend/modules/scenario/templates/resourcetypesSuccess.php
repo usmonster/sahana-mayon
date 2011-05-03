160645fc8d623d541c74055bea922baa6de1ef29
Darlene E McCullough <darlene.mccullough@mail.cuny.edu>
2011-05-03 14:15:28 -0400

    cleaning up early scenario pages

--- apps/frontend/modules/scenario/templates/resourcetypesSuccess.php
+++ apps/frontend/modules/scenario/templates/resourcetypesSuccess.php
@@ -1,27 +1,29 @@
 <?php
-  use_javascript('agMain.js');
+use_javascript('agMain.js');
+use_javascript('jquery.ui.custom.js');
+use_stylesheet('jquery/jquery.ui.custom.css');
+use_stylesheet('jquery/mayon.jquery.ui.css');
+use_javascript('agTooltip.js');
 ?>
 <script language="Javascript" type="text/javascript"> 
-    $(document).ready(function(){
-        var maxHeight=0;
+  $(document).ready(function(){
+    var maxHeight=0;
 
-        $('div.inlineLists').each(function(){
-          maxHeight = Math.max(maxHeight, $(this).height());
-        });
+    $('div.inlineLists').each(function(){
+      maxHeight = Math.max(maxHeight, $(this).height());
+    });
 
-       $('div.inlineLists').height(maxHeight);
-    })
+    $('div.inlineLists').height(maxHeight);
+  })
 
 </script>
-<h2>Edit Required Resource Types for Scenario</h2> <br />
+<h2>Select Resource Types: <span class="highlightedText"><?php echo $scenarioName ?> </span></h2> <br />
 <?php
-  include_partial('wizard', array('wizardDiv' => $wizardDiv));
+include_partial('wizard', array('wizardDiv' => $wizardDiv));
 ?>
-<p>Scenarios are plans for emergency responders.  Using the Scenario Creator you'll add facilities,
-  staff, and other resources to your plan.  For now, name the Scenario and give it a brief
-  description.</p>
+<p>Select the Staff and Facility Resources to be used in the <span class="highlightedText"><?php echo $scenarioName;
+?> </span> scenario. <a href="<?php echo url_for('@wiki') . '/doku.php?id=tooltip:resource_definition&do=export_xhtmlbody' ?>" class="tooltipTrigger" title="What is a Resource?">?</a>
+<br>Click 'Save and Continue' when you are ready to move forward.</p>
 
-<?php include_partial('resourceForm', array('resourceForm' => $resourceForm,'scenario_id' => $scenario_id)) ?>
 
-<p> After you've completed the description, click "Save and Continue" to move to the next step in
-  creating the Scenario.</p>
+<?php include_partial('resourceForm', array('resourceForm' => $resourceForm, 'scenario_id' => $scenario_id)) ?>
