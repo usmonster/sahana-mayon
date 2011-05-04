26a295acbcafde1140782957e9266cc350a62336
Darlene E McCullough <darlene.mccullough@mail.cuny.edu>
2011-05-04 12:07:08 -0400

    updating scenario fixtures and scenario wizard labels

--- apps/frontend/modules/scenario/lib/agScenarioWizard.class.php
+++ apps/frontend/modules/scenario/lib/agScenarioWizard.class.php
@@ -21,12 +21,12 @@ class agScenarioWizard
   {
     $this->steps = array(
       1 => array('title' => 'Name and Describe Scenario', 'url' => 'scenario/meta'),
-      2 => array('title' => 'Define needed Staff and Facility Types', 'url' => 'scenario/resourcetypes'),
+      2 => array('title' => 'Select Staff and Facility Types', 'url' => 'scenario/resourcetypes'),
       3 => array('title' => 'Create Facility Groups', 'url' => 'scenario/fgroup'),
-      4 => array('title' => 'Staff Resource Requirements', 'url' => 'scenario/staffresources'),
-      5 => array('title' => 'Create Staff Pools', 'url' => 'scenario/staffpool' ),
-      6 => array('title' => 'Shift Templates & Shift Definition', 'url' => 'scenario/shifttemplates'),
-      7 => array('title' => 'Shift Review ', 'url' => 'scenario/shifts'),
+      4 => array('title' => 'Set Staff Resource Requirements', 'url' => 'scenario/staffresources'),
+      5 => array('title' => 'Define Staff Pool', 'url' => 'scenario/staffpool' ),
+      6 => array('title' => 'Create Shift Templates', 'url' => 'scenario/shifttemplates'),
+      7 => array('title' => 'Review Shifts ', 'url' => 'scenario/shifts'),
       8 => array('title' => 'Scenario Review ', 'url' => 'scenario/review')
     );
 
