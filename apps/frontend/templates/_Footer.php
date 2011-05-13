c4d3d2de36b8602e2c0d0944c9925dd9ad8a0f58
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-05-13 13:55:44 -0400

    IE6 footer transparency fix (needs to be tested in IE6)

--- apps/frontend/templates/_Footer.php
+++ apps/frontend/templates/_Footer.php
@@ -1,5 +1,6 @@
     <div id="footer">
-      <?php echo image_tag('agasti_logo.png', array('alt' => '[Agasti Logo]')); ?>
+      <!--<?php echo image_tag('agasti_logo.png', array('alt' => '[Agasti Logo]','class' =>'footer_logo')); ?>-->
+      <div class="footer_logo"></div>
       <?php echo image_tag('logo.png', array('alt' => '[The Seal of the City of New York]')); ?>
     </div>
     <?php include_javascripts(); ?>
