85752a403ba0ce198c063a37d523701cc6dff2f7
charles wisniewski <charles.wisniewski@mail.cuny.edu>
2011-05-27 17:10:32 -0400

    staff search results fixed, all data present, with tooltips

--- apps/frontend/modules/search/templates/searchSuccess.php
+++ apps/frontend/modules/search/templates/searchSuccess.php
@@ -1,27 +1,28 @@
 <?php
-//actions.class.php global list actions
-
-//template
+   use_javascript('jquery.ui.custom.js');
+   use_stylesheet('jquery/jquery.ui.custom.css');
+  
 (isset($status)) ? $status = $status : $status = 'active';
 (isset($sort)) ? $sort = $sort : $sort = '';
 (isset($order)) ? $order = $order : $order = '';
-?>
-
-
-
-<?php
 
 if($sf_request->getParameter('module') == 'agStaff' || $sf_request->getParameter('module') == 'home' ) {
-$displayColumns = array(
-  'fn'              => array('title' => 'First Name', 'sortable' => false),
-  'ln'              => array('title' => 'Last Name', 'sortable' => false),
-  'agency'          => array('title' => 'Agency', 'sortable' => true),
-  'classification'  => array('title' => 'Classification', 'sortable' => true),
-  'phones'          => array('title' => 'Phone Contact(s)', 'sortable' => true),
-  'emails'          => array('title' => 'Email Contact(s)', 'sortable' => true),
-
-  'staff_status' => array('title' => 'Status', 'sortable' => false),
+  //set tooltips
+  $statusTooltip = url_for('@wiki') . '/doku.php?id=tooltip:staff_list_resource_status&do=export_xhtmlbody';
+  $orgTooltip = url_for('@wiki') . '/doku.php?id=tooltip:organization&do=export_xhtmlbody';
+  $resourceTooltip = url_for('@wiki') . '/doku.php?id=tooltip:staff_resource&do=export_xhtmlbody';
+  
+  $displayColumns = array(
+  'id' => array('title' => '', 'sortable' => false),
+  'fn' => array('title' => 'First Name', 'sortable' => true),
+  'ln' => array('title' => 'Last Name', 'sortable' => true),
+  'organization' => array('title' => 'Organization', 'sortable' => true, 'tooltip' => $orgTooltip),
+  'resource' => array('title' => 'Resource', 'sortable' => true, 'tooltip' => $resourceTooltip),
+  'phones' => array('title' => 'Phone', 'sortable' => false),
+  'emails' => array('title' => 'Email', 'sortable' => false),
+  'staff_status' => array('title' => 'Status', 'sortable' => false, 'tooltip' => $statusTooltip),
 );
+
 
 //pager comes in from the action
 
@@ -30,6 +31,7 @@ include_partial('global/list', array( 'sf_request' => $sf_request,
                                       'pager' => $pager,
                                       'order' => $order,
                                       'sort' => $sort,
+                                      'limit' => $limit,
                                       'status' => $status,
                                       'target_module' => $target_module,
                                       'caption' => 'Staff Search Results',
