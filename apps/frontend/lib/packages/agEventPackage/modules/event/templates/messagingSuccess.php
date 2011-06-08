7615d7fa2ba2b662e4cff57e72616dc0265bb6da
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-06-08 16:34:39 -0400

    layout change for event messaging page.

--- apps/frontend/lib/packages/agEventPackage/modules/event/templates/messagingSuccess.php
+++ apps/frontend/lib/packages/agEventPackage/modules/event/templates/messagingSuccess.php
@@ -1,5 +1,6 @@
 <?php
 use_javascript('agasti.js');
+use_javascript('agastiMessaging.js');
 use_javascript('jQuery.fileinput.js');
 use_javascript('jquery.ui.custom.js');
 use_stylesheet('jquery/jquery.ui.custom.css');
@@ -9,62 +10,85 @@ use_stylesheet('jquery/mayon.jquery.ui.css');
 <h2>Staff Messaging: <span class="highlightedText"><?php echo $event_name; ?> </span></h2>
 
 <p>Use this page to send messages and receive responses to your staff for <span class="highlightedText"><?php echo $event_name; ?></span>.</p>
-<?php
-  $exportUrl = url_for('event/exportcontacts?event=' . urlencode($sf_data->getRaw('event_name'))) ;
-  echo link_to('Export Unconfirmed Staff Contact List', $exportUrl, array('class' => 'generalButton')); 
-  //on click of this button, set the div content to 'exporting data, please wait'
-if(isset($exportComplete)){
-  echo $exportComplete . ' staff records exported, please send this file to your messaging vendor.';
-}
-  ?>
 
-<br />
-<br />
-<span style="display: inline-block; margin: 0px; padding: 0px" >
-<?php
-  $importUrl = url_for('event/importreplies?event=' . urlencode($sf_data->getRaw('event_name'))) ;
-  echo link_to('Import Staff Responses', $importUrl, 
-      array('class' => 'generalButton', 'title' => 'Import Staff', 'id' => 'import'));
-  
-  $wikiUrl =  url_for('@wiki') . '/doku.php?id=tooltip:staff_import&do=export_xhtmlbody';
-  echo link_to('?', $wikiUrl, 
-      array('class' => 'tooltipTrigger', 'title' => 'Importing Staff Replies', 'id' => 'import'));
-  $foo = new agStaff();
-?>
-  
-  <form id="importForm" style="position: relative; display: inline-block" action="
-  <?php echo $importUrl ?>" method="post" enctype="multipart/form-data">
-    < <div style="position: absolute; top: 0px; left: 0px; width: 250px">
-      <input  style="display: inline-block; color: #848484" class="inputGray" id="show" />
-      <a class="continueButton fileUploadBrowse" style="padding: 5px;">Browse</a>
-    </div>
+<table class="blueTable">
+    <tr class="head">
+        <th class="row1">Steps</th>
+        <th>Description</th>
+    </tr>
+    <tr>
+        <td>
 
-    <input type="file" name="import" id="fileUpload" style="height:25px" />
+            <?php
+            $exportUrl = url_for('event/exportcontacts?event=' . urlencode($sf_data->getRaw('event_name')));
+            echo link_to('Export Unconfirmed Staff Contact List', $exportUrl, array('class' => 'generalButton'));
+            //on click of this button, set the div content to 'exporting data, please wait'
+            if (isset($exportComplete)) {
+                echo $exportComplete . ' staff records exported, please send this file to your messaging vendor.';
+            }
+            ?></td>
+        <td> description</td>
+    </tr>
+    <tr>
+        <td>
+
+            <span style="display: inline-block; margin: 0px; padding: 0px" >
+                <?php
+                $importUrl = url_for('event/importreplies?event=' . urlencode($sf_data->getRaw('event_name')));
+                echo link_to('Import Staff Responses', $importUrl,
+                        array('class' => 'generalButton', 'title' => 'Import Staff', 'id' => 'import'));
+
+                $wikiUrl = url_for('@wiki') . '/doku.php?id=tooltip:staff_import&do=export_xhtmlbody';
+                echo link_to('?', $wikiUrl,
+                        array('class' => 'tooltipTrigger', 'title' => 'Importing Staff Replies', 'id' => 'import'));
+                $foo = new agStaff();
+                ?>
 
 
-    <input type="submit" name="submit" value="Submit" class="submitLinkButton" style="position:absolute; top:0px; left: 199px"/>
-  </form>
-</span>
-<br />
-<br />
-<a href="#" class="generalButton">Preview Confirmed Staff Pool</a>
-<br />
-<br />
+            </span></td>
+        <td>
+            <span id="descText">testing</span>
+            <form id="importForm" style="position: relative; display: inline-block" action="
+                  <?php echo $importUrl ?>" method="post" enctype="multipart/form-data">
+                <div style="position: absolute; top: 0px; left: 0px; width: 250px">
+                    <input  style="display: inline-block; color: #848484" class="inputGray" id="show" />
+                    <a class="continueButton fileUploadBrowse" style="padding: 5px;">Browse</a>
+                </div>
 
-<?php
-  $deployUrl = url_for('event/deploystaff?event=' . urlencode($sf_data->getRaw('event_name'))) ;
-  echo link_to('Deploy Staff to Facilities', $deployUrl, array('class' => 'generalButton')); 
-?>
-<br />
-<br />
+                <input type="file" name="import" id="fileUpload" style="height:25px" />
 
-<?php
 
-  echo link_to('Export All Staff Contact List', $exportUrl . '&all', array('class' => 'generalButton'));
-  //on click of this button, set the div content to 'exporting data, please wait'
-if(isset($exportComplete)){
-  echo $exportComplete . ' staff records exported, please send this file to your messaging vendor.';
-}
-  ?>
-<br />
-<br />
+                <input type="submit" name="submit" value="Submit" class="submitLinkButton" style="position:absolute; top:0px; left: 199px"/>
+            </form>
+        </td>
+    </tr>
+    <tr>
+        <td>
+
+            <a href="#" class="generalButton">Preview Confirmed Staff Pool</a>
+        </td>
+        <td> description</td>
+    </tr>
+    <tr>
+        <td>
+
+            <?php
+                  $deployUrl = url_for('event/deploystaff?event=' . urlencode($sf_data->getRaw('event_name')));
+                  echo link_to('Deploy Staff to Facilities', $deployUrl, array('class' => 'generalButton'));
+            ?>
+              </td>
+              <td> description</td>
+          </tr>
+          <tr>
+              <td>
+
+            <?php
+                  echo link_to('Export All Staff Contact List', $exportUrl . '&all', array('class' => 'generalButton'));
+                  //on click of this button, set the div content to 'exporting data, please wait'
+                  if (isset($exportComplete)) {
+                      echo $exportComplete . ' staff records exported, please send this file to your messaging vendor.';
+                  }
+            ?>
+        </td>
+        <td> description</td>
+    </tr></table>
