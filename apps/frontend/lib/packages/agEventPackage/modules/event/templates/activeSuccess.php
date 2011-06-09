d6408b6dad2192f7a072f2cf53da9b54adebe0c0
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-06-09 15:23:34 -0400

    event active page update... Show link to Event Name Zero Hour when the event is in pre-deployment state.(needs to be tested)

--- apps/frontend/lib/packages/agEventPackage/modules/event/templates/activeSuccess.php
+++ apps/frontend/lib/packages/agEventPackage/modules/event/templates/activeSuccess.php
@@ -9,84 +9,90 @@ $urlEncodedEventName = urlencode($sf_data->getRaw('event_name'));
 if (isset($blackOutFacilities)) {
 ?>
 
-  <p><span class="highlightedText">Please Note!</span> the shifts at these facilities, in these facility groups have not been affected (their activation times have not been changed).</p>
-  <a href="<?php echo url_for('event/fgroup?event=' . $urlEncodedEventName) ?>" class="continueButton" title="Facilities and Resources">Maually set Facility Resource Activation Times</a><br/>
+    <p><span class="highlightedText">Please Note!</span> the shifts at these facilities, in these facility groups have not been affected (their activation times have not been changed).</p>
+    <a href="<?php echo url_for('event/fgroup?event=' . $urlEncodedEventName) ?>" class="continueButton" title="Facilities and Resources">Maually set Facility Resource Activation Times</a><br/>
 <?php
-  foreach ($blackOutFacilities as $facilityKey => $facility) {
+    foreach ($blackOutFacilities as $facilityKey => $facility) {
 
-  }
+    }
 }
 ?>
 
 
-<!--<a href="<?php echo url_for('event/meta?event=' . $urlEncodedEventName); ?>" class="continueButton" title="Change Event Metadata">Change Event Metadata</a><br/><br/>
-
-<a href="<?php echo url_for('event/staff?event=' . $urlEncodedEventName); ?>" class="continueButton" class="continueButton" title="Manage Event Staff">Staff Management</a><br/>
-
-<em><a href="<?php echo url_for('agClient/index'); ?>" class="buttonText" title="Manage Clients">Client Data</a></em><br/>
-
-<a href="<?php echo url_for('event/listgroups?event=' . $urlEncodedEventName) ?>" class="continueButton" title="Facilities and Resources">Manage Facility Groups</a><br/>
-
-<em><a href="<?php echo url_for('event/reporting'); ?>" class="buttonText" title="Manage Reports">Reporting</a></em><br/>
-<br>-->
-
-
 <br/>
 <table class="blueTable">
-  <tr class="head">
-    <th class="row1">Actions</th>
-    <th>Description</th>
-  </tr>
-  <tr>
-    <td>
-      <a class="buttonText" href="<?php echo url_for('event/meta?event=' . $urlEncodedEventName); ?>">Event Name and Zero Hour</a>
-    </td>
-    <td>Name: <span class="highlightedText"><?php echo $event_name ?></span>
-      <br />Description: <span class="highlightedText"><?php echo $event_description ?></span>
-    </td>
-  </tr>
-  <tr>
-    <td>
-      <a class="buttonText" href="<?php echo url_for('event/messaging?event=' . $urlEncodedEventName); ?>">Event Staff Messaging</a>
-    </td>
-    <td>
-      Total Staff: <span class="highlightedText"><?php echo $eventStaffPool ?></span><br />
-      Confirmed Staff: <span class="highlightedText"><?php echo $eventAvailableStaff ?></span>
-    </td>
-  </tr>
-  <tr>
-    <td>
-      <a class="buttonText" href="<?php echo url_for('event/staff?event=' . $urlEncodedEventName); ?>">Event Staff Management</a>
-    </td>
-    <td>
-      Total Staff: <span class="highlightedText"><?php echo $eventStaffPool ?></span>
-    </td>
-  </tr>
-  <tr>
-    <td>
-      <a class="buttonText" href="<?php echo url_for('event/listgroups?event=' . $urlEncodedEventName); ?>">Event Facility Management</a>
-    </td>
-    <td>
-      Facility Groups: <span class="highlightedText"><?php echo $event_facility_groups ?></span><br /> 
-      Facility Resources: <span class="highlightedText"><?php echo $event_facility_resources ?></span>
-    </td>
-  </tr>
-  <tr>
-    <td>
-      <a class="buttonText" href="#">Client Information</a>
-    </td>
-    <td>
-      <div style="font-style:oblique">No statistical data is currently available.</div>
-    </td>
-  </tr>
-  <tr>
-    <td>
-      <a class="buttonText" href="#">Reporting</a>
-    </td>
-    <td>
-      <div style="font-style:oblique">No statistical data is currently available.</div>
-    </td>
-  </tr>
+    <tr class="head">
+        <th class="row1">Actions</th>
+        <th>Description</th>
+    </tr>
+
+    <?php
+    $current_status = agEventFacilityHelper::returnCurrentEventStatus($event_id);
+    if ($current_status != "") {
+        $cur_status = Doctrine::getTable('agEventStatusType')
+                        ->findByDql('id = ?', $current_status)
+                        ->getFirst()->event_status_type;
+
+        $upper_case_cur_Status = strtoupper($cur_status);
+        if ($upper_case_cur_Status == "PRE-DEPLOYMENT") {
+            //echo $cur_status;
+
+            echo "<tr>
+        <td>
+        <a class=\"buttonText\" href=\"<?php echo url_for('event/meta?event=' . $urlEncodedEventName); ?>\">Event Name and Zero Hour</a>
+        </td>
+        <td>Name: <span class=\"highlightedText\"><?php echo $event_name
+?></span>
+
+            </td>
+          </tr>";
+            //<br />Description: <span class=\"highlightedText\"><?php echo $event_description
+        }
+    }
+    ?>
+
+    <tr>
+        <td>
+            <a class="buttonText" href="<?php echo url_for('event/messaging?event=' . $urlEncodedEventName); ?>">Event Staff Messaging</a>
+        </td>
+        <td>
+            Total Staff: <span class="highlightedText"><?php echo $eventStaffPool ?></span><br />
+            Confirmed Staff: <span class="highlightedText"><?php echo $eventAvailableStaff ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <a class="buttonText" href="<?php echo url_for('event/staff?event=' . $urlEncodedEventName); ?>">Event Staff Management</a>
+        </td>
+        <td>
+            Total Staff: <span class="highlightedText"><?php echo $eventStaffPool ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <a class="buttonText" href="<?php echo url_for('event/listgroups?event=' . $urlEncodedEventName); ?>">Event Facility Management</a>
+        </td>
+        <td>
+            Facility Groups: <span class="highlightedText"><?php echo $event_facility_groups ?></span><br />
+            Facility Resources: <span class="highlightedText"><?php echo $event_facility_resources ?></span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <a class="buttonText" href="#">Client Information</a>
+        </td>
+        <td>
+            <div style="font-style:oblique">No statistical data is currently available.</div>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <a class="buttonText" href="#">Reporting</a>
+        </td>
+        <td>
+            <div style="font-style:oblique">No statistical data is currently available.</div>
+        </td>
+    </tr>
 </table>
 <br />
 <br />
