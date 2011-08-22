4ed21b8dd105cdd3378b922fa497dc1e04cc0dca
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-08-22 14:31:48 -0400

    printable event staff summary page.

--- apps/frontend/lib/packages/agEventPackage/modules/event/templates/_staffingsummary.php
+++ apps/frontend/lib/packages/agEventPackage/modules/event/templates/_staffingsummary.php
@@ -3,26 +3,26 @@
   if (!empty($results)): // print_r($results);
 ?>
 <?php foreach ($results as $efg_id => $groupinfo): ?>
-      <div style="background-color:#ccc; display:inline-block;">
+      <div style="background-color:#ccc; display:inline-block; border:1px solid #ccc">
         <div style="display:inline-block;padding:10px 10px 5px; font-size:17px; color:#fff; font-weight:bold "> Facility Group: <?php echo $groupinfo['group_name']; ?></div>
         <div style="display:inline-block;float:right; padding:10px 10px 5px; font-size:17px; color:#fff; font-weight:bold ">Status: <?php echo $groupinfo['group_status']; ?></div>
         <br>
   <?php foreach ($groupinfo['facilities'] as $efr_id => $facilityinfo): ?>
-        <div style="background-color:#d8d8d8; display:inline-block; margin:3px 10px;padding:5px;width:auto; border:1px solid #fff; ">
+        <div style="background-color:#d8d8d8; display:inline-block; margin:3px 10px;padding:5px;width:auto; border:1px solid #999; ">
           <div style="display:inline-block; text-align:left;padding:5px 20px 5px 5px;">Facility: <?php echo $facilityinfo['facility_type'] ?>-<?php echo $facilityinfo['facility_code'] ?></div><div style="display:inline-block;padding:5px 20px 5px 10px; ">Facility Name: <?php echo $facilityinfo['facility_name'] ?></div><div style="display:inline-block;padding:5px; float:right ">Facility Status: <?php echo $facilityinfo['facility_status']; ?></div>
           <br>
-          <table class="blueTable" cellpadding="5" style="width:auto">
+          <table class="blueTable" cellpadding="5" style="width:auto; border: 1px solid #dadada">
             <tr class="head"><th>Staff Type</th><th>Staff Count</th><th>Min / Max</th><th>Shift Status</th><th>Staff Wave</th><th>Shift Start</th><th>Break Start</th><th>Shift End</th><th>Time Zone</th></tr>
       <?php foreach ($facilityinfo['shifts'] as $es_id => $shiftinfo): ?>
       <?php if ($shiftinfo['shift_disabled'] || $shiftinfo['shift_standby']): ?>
-            <tr style="background-color:#EEE; padding:5px; ">
+            <tr style="background-color:#EEE; padding:5px; border:1px solid #dadada">
         <?php else: ?>
-            <tr style="background-color:#fff; padding:5px;  ">
+            <tr style="background-color:#fff; padding:5px; border:1px solid #dadada">
         <?php endif; ?>
               <td><?php echo $shiftinfo['staff_type']; ?></td>
               <td>
           <?php if ($shiftinfo['shift_disabled']): ?>
-                <span style="color: #999">
+                <span style="color: #999; border:1px solid #dadada">
             <?php else: ?>
             <?php
                   if ($shiftinfo['staff_count'] == $shiftinfo['maximum_staff']) {
@@ -40,7 +40,7 @@
               </tr>
       <?php endforeach; ?>
                 </table>
-              </div><br><br>
+              </div><br> 
   <?php endforeach; ?>
                 </div><br><br><br>
 <?php endforeach; ?>
