a8a298c8a6d57450c44263d5eae78d38b9730b41
Pradeep Vijayagiri <pradeep.vijayagiri@mail.cuny.edu>
2011-08-25 16:34:13 -0400

    Staff Summary page toggling facility groups (Completed)

--- web/js/menu-test.js
+++ web/js/menu-test.js
@@ -1,47 +1,3 @@
-/* 
-Simple JQuery menu.
-HTML structure to use:
-
-Notes: 
-
-1: each menu MUST have an ID set. It doesn't matter what this ID is as long as it's there.
-2: each menu MUST have a class 'menu' set. If the menu doesn't have this, the JS won't make it dynamic
-
-Optional extra classnames:
-
-noaccordion : no accordion functionality
-collapsible : menu works like an accordion but can be fully collapsed
-expandfirst : first menu item expanded at page load
-
-<ul id="menu1" class="menu [optional class] [optional class]">
-<li><a href="#">Sub menu heading</a>
-<ul>
-<li><a href="http://site.com/">Link</a></li>
-<li><a href="http://site.com/">Link</a></li>
-<li><a href="http://site.com/">Link</a></li>
-...
-...
-</ul>
-<li><a href="#">Sub menu heading</a>
-<ul>
-<li><a href="http://site.com/">Link</a></li>
-<li><a href="http://site.com/">Link</a></li>
-<li><a href="http://site.com/">Link</a></li>
-...
-...
-</ul>
-...
-...
-</ul>
-
-Copyright 2008 by Marco van Hylckama Vlieg
-
-web: http://www.i-marco.nl/weblog/
-email: marco@i-marco.nl
-
-Free for non-commercial use
-*/
-
 function initMenus() {
 		
 	$('ul.menu_summary ul').hide();
@@ -55,14 +11,14 @@ function initMenus() {
 			$('ul.menu_summary ul').show('slow');
 			$('ul.menu_summary li a').addClass('selectedListItem');
 			$('ul.menu_summary li a span.link-arrow').html("&#9650;"); //up arrow
-			
-		 
+			$('ul.menu_summary li div.grp_sumry').hide();
 			      });
 	$('div a.collapseall').click(
 		function() {
 			$('ul.menu_summary ul').hide('slow');
 			$('ul.menu_summary li a').removeClass('selectedListItem');
 			$('ul.menu_summary li a span.link-arrow').html("&#9660;"); //down arrow
+      $('ul.menu_summary li div.grp_sumry').show();
 			     });
 	
 	//end toggle functionality
@@ -73,13 +29,14 @@ function initMenus() {
 			$('#' + this.id + '.expandfirst ul:first').show();
 			$('#' + this.id + '.expandfirst a:first').addClass('selectedListItem');
 			$('#' + this.id + '.expandfirst a:first span.link-arrow').html("&#9650;"); //up arrow
+      $('#' + this.id + '.expandfirst ul:first').parent().children('div.grp_sumry').hide();
 		 
 		}
 		else {			
 			$('#' + this.id + ' .' + cookie).next().show();
 			$('#' + this.id + ' .' + cookie).addClass('selectedListItem');
 			$('#' + this.id + ' .' + cookie + ' span.link-arrow').html("&#9650;"); //up arrow
-			
+			$('#' + this.id + ' .' + cookie).parent().children('div.grp_sumry').hide();
 		}
 		
 		//alert(cookie);
@@ -105,6 +62,7 @@ function initMenus() {
 					$(this).next().slideToggle('normal');
 					 $(this).toggleClass('selectedListItem');
 					 $(this).children('span.link-arrow').html($(this).hasClass('selectedListItem') ? '&#9650;' : '&#9660;');
+           $(this).parent().children('div.grp_sumry').toggle();
 				}				
 			}
 		} 
