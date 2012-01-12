d8e2c6d5bba51960a2cae1071b5ed4c11e21503f
Chad Heuschober <chad.heuschober@mail.cuny.edu>
2012-01-12 10:11:44 -0500

    Fixed graph display.

--- apps/frontend/lib/packages/agEventPackage/modules/event/templates/_staffingestimates.php
+++ apps/frontend/lib/packages/agEventPackage/modules/event/templates/_staffingestimates.php
@@ -1,4 +1,6 @@
 <?php
+  // this file has some odd divs at the end, you have to pay attention to the loops for 2-columns
+  // it really is correct, the IDE just can't tell
   $uniqStaffCounts = $sf_data->getRaw('uniqStaffCounts');
   if (!empty($uniqStaffCounts)):
 ?>
@@ -46,7 +48,8 @@ The following are staffing estimates at <?php echo date('Y-m-d H:i:s T', $report
 <?php endif; ?>
 </div>
 
-<?php if(!empty($staffTypeEstimates)): ?>
+<?php $data = $sf_data->getRaw('staffTypeEstimates');
+  if(!empty($data)): ?>
 <div class="infoHolder width750Px">
   <h3>Staff Type Estimates</h3>
   <br />
@@ -97,24 +100,25 @@ The following are staffing estimates at <?php echo date('Y-m-d H:i:s T', $report
   <br />
 
   <div class="pad1Em">
-  <?php $i = 0; foreach ($staffTypeEstimates AS $typeId => $se): ?>
-    <?php if ($i % 2 == 0): ?>
-      <div>
-    <?php endif; ?>
-    <?php $i++; ?>
-    <div class="graphDisplay padding20Px">
-      <a title="<?php echo $se['resource_type']; ?>" name="st<?php echo $typeId; ?>"><h5><?php echo $se['resource_type']; ?></h5></a>
-      <img src="<?php echo $pCharts['staffTypeStatusPie'][$typeId] ?>" alt="<?php echo $se['resource_type']; ?> Status Pie Chart">
-    </div>
-    <?php if ($i % 2 != 0): ?>
+  <?php
+    $i = 0;
+    $ct = count($staffTypeEstimates);
+    foreach ($staffTypeEstimates AS $typeId => $se):  ?>
+      <?php $i++; ?>
+      <?php if ($i % 2 != 0): ?>
+        <div>
+      <?php endif; ?>
+      <div class="graphDisplay padding20Px">
+        <a title="<?php echo $se['resource_type']; ?>" name="st<?php echo $typeId; ?>"><h5><?php echo $se['resource_type']; ?></h5></a>
+        <img src="<?php echo $pCharts['staffTypeStatusPie'][$typeId] ?>" alt="<?php echo $se['resource_type']; ?> Status Pie Chart">
       </div>
-    <?php endif; ?>
+      <?php if ($i % 2 == 0 || $i == $ct): ?>
+        </div>
+      <?php endif; ?>
   <?php endforeach; ?>
-  <?php if ($i % 2 == 0): ?>
-    </div>
-  <?php endif; ?>
+  <div class="clearBoth"></div>
   </div>
+</div>
 <?php endif; ?>
 
-<div class="clearBoth"></div>
 <?php endif; ?>
